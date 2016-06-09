1fefee11066eac6297cc8a9752047ccd3a8620f7
Victor <ychtan@email.gwu.edu>
2016-06-09 15:46:05 -0400

    [27. Remove Element][Accepted]committed by Victor

---
+++ 27-Remove-Element/solution_pythonic.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        nums[:] = filter(lambda x: x != val, nums)
+        return len(nums)
