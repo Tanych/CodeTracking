acf8326a208a5114b1695b6e9ce90408a47afda9
Victor <ychtan@email.gwu.edu>
2016-06-15 15:35:46 -0400

    [283. Move Zeroes][Time Limit Exceeded]committed by Victor

---
+++ 283-Move-Zeroes/solution.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def moveZeroes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+        i=0
+        while i<len(nums):
+            if nums[i]==0:
+                j=i+1
+                while j<len(nums):
+                    if nums[j]!=0:
+                        # swap
+                        nums[i],nums[j]=nums[j],nums[i]
+                        break
+                    j+=1
+                i+=1
