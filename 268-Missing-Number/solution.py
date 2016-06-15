9a9e85ee17a4f541dc677576a6f12706a644e1d5
Victor <ychtan@email.gwu.edu>
2016-06-15 16:08:14 -0400

    [268. Missing Number][Accepted]committed by Victor

--- 268-Missing-Number/solution.py
+++ 268-Missing-Number/solution.py
@@ -4,6 +4,4 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        if not filter(lambda x:x==0, nums):
-            return 0
         return sum(i for i in xrange(len(nums)+1))-sum(nums)
