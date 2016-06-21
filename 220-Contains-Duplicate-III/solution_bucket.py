fdc976ad93d99f21a3442c149acd514ed1b1e03c
Victor <ychtan@email.gwu.edu>
2016-06-21 17:20:28 -0400

    [220. Contains Duplicate III][Time Limit Exceeded]committed by Victor

---
+++ 220-Contains-Duplicate-III/solution_bucket.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def getBucketID(self, i, w):
+        return (i + 1) / w - 1 if i<0 else i/w
+        
+    def containsNearbyAlmostDuplicate(self, nums, k, t):
+        """
+        :type nums: List[int]
+        :type k: int
+        :type t: int
+        :rtype: bool
+        """
+        n=len(nums)
+        if n<2 or k<1 or t<0:
+            return False
+        buckets={}
+        width=t+1
+        for i in xrange(n):
+            id=self.getBucketID(nums[i],width)
+            if id in buckets.keys():
+                return True
+            if (id-1 in buckets.keys() and nums[i]-buckets[id-1]<width) or \
+                    (id+1 in buckets.keys() and buckets[id+1]-nums[i]<width):
+                return True
+            buckets[id] = nums[i]
+            if i>=k:
+                del buckets[self.getBucketID(nums[i - k], width)]
+        return False
