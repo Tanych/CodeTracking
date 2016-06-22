85332b90bd0632f1ab93742b8cd0f38b4f2f7586
Victor <ychtan@email.gwu.edu>
2016-06-22 15:50:43 -0400

    [220. Contains Duplicate III][Accepted]committed by Victor

--- 220-Contains-Duplicate-III/solution.py
+++ 220-Contains-Duplicate-III/solution.py
@@ -10,18 +10,24 @@ class Solution(object):
         :rtype: bool
         """
         n=len(nums)
+        # kick out the special
         if n<2 or k<1 or t<0:
             return False
+        # the buckets has a width with t+1, the nums[i], nums[j] has two situation
+        # p1: in the same bucket, p2: in the neighor bucket
         buckets={}
         width=t+1
         for i in xrange(n):
             id=self.getBucketID(nums[i],width)
+            # nums[i], nums[j] in the same buckets
             if id in buckets:
                 return True
-            if (id-1 in buckets and abs(nums[i]-buckets[id-1]<width)) or \
-                    (id+1 in buckets and abs(buckets[id+1]-nums[i]<width)):
+            # nums[i], nums[j] in the neighor buckets
+            if (id-1 in buckets and nums[i]-buckets[id-1]<width) or \
+                    (id+1 in buckets and buckets[id+1]-nums[i]<width):
                 return True
             buckets[id] = nums[i]
+            # if |i-j|>k, then kick out the k before element
             if i>=k:
                 del buckets[self.getBucketID(nums[i - k], width)]
         return False
