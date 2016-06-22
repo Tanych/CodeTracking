63223a70df6b5583e2ff1db1b4744a417a019278
Victor <ychtan@email.gwu.edu>
2016-06-22 17:09:52 -0400

    [209. Minimum Size Subarray Sum][Time Limit Exceeded]committed by Victor

---
+++ 209-Minimum-Size-Subarray-Sum/solution.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def minSubArrayLen(self, s, nums):
+        """
+        :type s: int
+        :type nums: List[int]
+        :rtype: int
+        """
+        left=right=min_len=0
+        num_len=len(nums)
+        cur_sum=nums[0] if nums else 0
+        
+        while right<num_len:
+            while cur_sum<s:
+                right+=1
+                # if reach the boundary return
+                if right==num_len:
+                    return min_len
+                cur_sum+=nums[right]
+                
+                if (right-left+1)<min_len or not min_len:
+                    min_len=right-left+1
+                # update left, kick the most left ele
+                cur_sum-=nums[left]
+                left+=1
+        return min_len
+        
