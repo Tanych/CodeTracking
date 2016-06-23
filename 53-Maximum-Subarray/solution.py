2c2996ec4489755ec4c0f4af7558ed7519a36d4b
Victor <ychtan@email.gwu.edu>
2016-06-23 11:23:42 -0400

    [53. Maximum Subarray][Accepted]committed by Victor

--- 53-Maximum-Subarray/solution.py
+++ 53-Maximum-Subarray/solution.py
@@ -5,13 +5,13 @@ class Solution(object):
         :rtype: int
         """
         # to make the sum as most as bigger, 
-        # we need to find i,j to make cursum[i]-cursum[j] as close to K
+        # we need to find i,j to make cursum[i]-cursum[j] as close to inf
         n=len(nums)
         maxsum=cursum=nums[0]
         for i in xrange(1,n):
             # if cursum[i-1] less than 0,no need to carry to current
             # make it recounter
-            cursum=(0 if cursum<0 else cursum)+nums[i]
+            cursum=max(nums[i], cursum+nums[i])
             # check the maxsum
-            maxsum=cursum if cursum>maxsum else maxsum
+            maxsum=max(cursum,maxsum)
         return maxsum
