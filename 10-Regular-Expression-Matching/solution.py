db5e1404c5e4e22fc80b17bac7c360a27a137627
Victor <ychtan@email.gwu.edu>
2016-07-03 17:09:13 -0400

    [10. Regular Expression Matching][Accepted]committed by Victor

---
+++ 10-Regular-Expression-Matching/solution.py
@@ -0,0 +1,31 @@
+class Solution(object):
+    def isMatch(self, s, p):
+        """
+        :type s: str
+        :type p: str
+        :rtype: bool
+        """
+        dp = [[False for j in range(len(p) + 1)] for i in range(len(s) + 1)]
+        dp[0][0] = True
+        # initial the value that a*b  value equal (disappear)b
+        for j in range(1,len(p) + 1):
+                if p[j-1] == '*':
+                    # since * can make the preivous disappear
+                    if j >= 2:
+                        dp[0][j] = dp[0][j - 2]
+        
+        for i in range(1, len(s) + 1):
+            for j in range(1, len(p) + 1):
+                # if preivous == or j-1 is . then match
+                if p[j - 1] == '.'or s[i-1] == p[j-1]:   ## first case 
+                    dp[i][j] = dp[i - 1][j - 1]
+                # if preivous is *, check the previous 
+                elif p[j - 1] == '*':                                           #aa *a case                     #.* case
+                    dp[i][j] = dp[i][j - 1] or dp[i][j - 2] or (dp[i-1][j] and (s[i-1] == p[j-2] or p[j-2] == '.'))
+                    
+        return dp[-1][-1]
+        
+            
+        
+            
+            
