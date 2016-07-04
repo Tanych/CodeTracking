b127ced991f8273cfccc715426caa16c92f16f33
Victor <ychtan@email.gwu.edu>
2016-07-03 20:22:40 -0400

    [10. Regular Expression Matching][Accepted]committed by Victor

--- 10-Regular-Expression-Matching/solution.py
+++ 10-Regular-Expression-Matching/solution.py
@@ -5,30 +5,32 @@ class Solution(object):
         :type p: str
         :rtype: bool
         """
-        dp = [[False for j in range(len(p) + 1)] for i in range(len(s) + 1)]
-        dp[0][0] = True
-        # initial the value that a*b  value equal (disappear)b
-        for j in range(1,len(p) + 1):
-                if p[j-1] == '*':
-                    # since * can make the preivous disappear
-                    if j >= 2:
-                        dp[0][j] = dp[0][j - 2]
+        # recursive to solve the problem
+        # if empty pattern
+        if not p:
+            return not s
         
-        for i in range(1, len(s) + 1):
-            for j in range(1, len(p) + 1):
-                # if preivous == or j-1 is . then match
-                if p[j - 1] == '.'or s[i-1] == p[j-1]:   ## first case 
-                    dp[i][j] = dp[i - 1][j - 1]
-                # if preivous is *, check the previous 
-                elif p[j - 1] == '*': 
-                    # match 0 ele; match 1 ele
-                    # match mutilple, for example 
-                    # src:abcdeeee dst:abcde*
-                    # check abceee match abcde* ==>check abcee match abcde* ==> and so on
-                    dp[i][j] = dp[i][j - 1] or dp[i][j - 2] or (dp[i-1][j] and (s[i-1] == p[j-2] or p[j-2] == '.'))
-                    
-        return dp[-1][-1]
-        
+        # if the last is '*'
+        if p[-1]=='*':
+            # abcdeeee
+            # abcde*
+            if s and (s[-1]==p[-2] or p[-2]=='.'):
+                # * match 0==>match(s,p[:-2])
+                # * match 1 or more==>match preivous s and whole p
+                return self.isMatch(s,p[:-2]) or self.isMatch(s[:-1],p)
+            else:
+                # abcdf
+                # abcdfe*
+                # only try to match abcdf abcdf
+                return self.isMatch(s,p[:-2])
+        else:
+            # abcde
+            # abcd.(e)
+            if s and (s[-1]==p[-1] or p[-1]=='.'):
+                return self.isMatch(s[:-1],p[:-1])
+            else:
+                # other return false
+                return False
             
         
             
