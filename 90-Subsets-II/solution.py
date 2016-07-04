a7d44c84c3c7bd61ab550cd534a290a69dec20a2
Victor <ychtan@email.gwu.edu>
2016-07-04 18:22:19 -0400

    [90. Subsets II][Accepted]committed by Victor

--- 90-Subsets-II/solution.py
+++ 90-Subsets-II/solution.py
@@ -5,17 +5,7 @@ class Solution(object):
         :rtype: List[List[int]]
         """
         nums.sort()
-        res=[]
-        n=len(nums)
-        for i in xrange(2**n):
-            subset=[]
-            # using the binary move
-            bn=bin(i)[2:]
-            # 001, make all the same length
-            bn='0'*(n-len(bn))+bn
-            for j in xrange(len(bn)):
-                if bn[j]=='1':
-                    subset.append(nums[j])
-            if subset not in res:
-                res.append(subset)
+        res=[[]]
+        for num in nums:
+            res+=[i +[num] for i in res if i +[num] not in res]
         return res
