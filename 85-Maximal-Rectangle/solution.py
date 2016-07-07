5845be5d3783c12ad6afac93ceb01ecbf27de341
Victor <ychtan@email.gwu.edu>
2016-07-06 22:41:59 -0400

    [85. Maximal Rectangle][Accepted]committed by Victor

--- 85-Maximal-Rectangle/solution.py
+++ 85-Maximal-Rectangle/solution.py
@@ -4,48 +4,26 @@ class Solution(object):
         :type matrix: List[List[str]]
         :rtype: int
         """
-        # This is a Brilliant DP solution from morrischen2008
-        # It try to record the left and right bounary of the rectange, also the height
-        # The max area would be (right-left)*height
+        # this method reflects the maximal histogram rect
+        # using the stack the record the highest height in previous 
         
-        row=len(matrix)
-        if row==0:
-            return 0
-            
-        col=len(matrix[0])
-        if col==0:
-            return 0
-            
-        # left,right bounary of the element and height of the element
-        height=[0]*col
-        right=[col]*col
-        left=[0]*col
-        # max area
+        height=[0 for _ in xrange(len(matrix[0]))] if matrix else None
+        # record the height
+      
         maxA=0
-        
-        for i in xrange(row):
-            cur_left=0
-            cur_right=col
-            # count the height and the left bounary
-            for j in xrange(col):
-                if matrix[i][j]=='1':
-                    height[j]+=1
-                    left[j]=max(left[j],cur_left)
-                else:
-                    height[j]=0
-                    left[j]=0
-                    cur_left=j+1
-            # count the right bounary
-            for j in xrange(col-1,-1,-1):
-                if matrix[i][j]=='1':
-                    right[j]=min(right[j],cur_right)
-                else:
-                    right[j]=col
-                    cur_right=j
-                    
-            # count the area
-            for j in xrange(col):
-                maxA=max(maxA,(right[j]-left[j])*height[j])
-                
+        for row in matrix:
+            stack=[]
+            # get the height record for each row
+            for cnt,ch in enumerate(row):
+                height[cnt]=(height[cnt]+1) if ch=='1' else 0
+            # get the max area in each row
+            for idx,ht in enumerate(height+[0]):
+                # count the area util the stack is the heighest
+                # pay attention to the while. it will continue count 
+                # until it stops at the height large than the new one
+                while stack and ht<height[stack[-1]]:
+                    th=height[stack.pop()]
+                    # count the max area
+                    maxA=max(maxA,th*(idx-stack[-1]-1 if stack else idx))
+                stack.append(idx)
         return maxA
-        
