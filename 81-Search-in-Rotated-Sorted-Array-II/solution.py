bd3ea018dedd6824665fec04935fe81468764202
Victor <ychtan@email.gwu.edu>
2016-07-07 15:42:26 -0400

    [81. Search in Rotated Sorted Array II][Accepted]committed by Victor

--- 81-Search-in-Rotated-Sorted-Array-II/solution.py
+++ 81-Search-in-Rotated-Sorted-Array-II/solution.py
@@ -7,7 +7,23 @@ class Solution(object):
         """
         # first lets do in naive way
         
-        if target in nums:
-            return True
-        else:
-            return False
+        n=len(nums)
+        left,right=0,n-1
+        while left<=right:
+            mid=left+(right-left)/2
+            if nums[mid]==target:
+                return True
+            if nums[mid]==nums[left]:
+                left+=1
+            elif nums[mid]>nums[left]:
+                if target>=nums[left] and target<nums[mid]:
+                    right=mid-1
+                else:
+                    left=mid+1
+            else:
+                if target<=nums[right] and target>nums[mid]:
+                    left=mid+1
+                else:
+                    right=mid-1
+        return False
+                    
