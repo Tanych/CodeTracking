27f8c5a6a9c900c2b90795cb66da340af989a812
Victor <ychtan@email.gwu.edu>
2016-07-07 15:52:14 -0400

    [33. Search in Rotated Sorted Array][Accepted]committed by Victor

--- 33-Search-in-Rotated-Sorted-Array/solution.py
+++ 33-Search-in-Rotated-Sorted-Array/solution.py
@@ -8,33 +8,25 @@ class Solution(object):
         # step1-- first find the pivot of the rotated array
         n=len(nums)
         left,right=0,n-1
-        
-        while left<right:
-            mid=left+(right-left)/2
-            if nums[mid]>nums[right]:
-                left=mid+1
-            else:
-                right=mid
-        pivot=right
-        
-        # search the target compare the real middle nums
-        left,right=0,n-1
         while left<=right:
+            print left,right
+            if right-left==1:
+                if nums[right]==target:
+                    return right
+                elif nums[left]==target:
+                    return left
             mid=left+(right-left)/2
-            # get the real middle index
-            realmid=(pivot+mid)%n
-            # find it
-            if nums[realmid]==target:
-                return realmid
-            # if target larger than realmid,
-            # it should be on the right part
-            # move left to mid
-            if nums[realmid]<target:
-                left=mid+1
-            # if target less than realmid
-            # if should be on the left part
-            # move to right to mid
+            if nums[mid]==target:
+                return mid
+            if nums[mid]>nums[left]:
+                if target>=nums[left] and target<nums[mid]:
+                    right=mid-1
+                else:
+                    left=mid+1
             else:
-                right=mid-1
-                
+                if target<=nums[right] and target>nums[mid]:
+                    left=mid+1
+                else:
+                    right=mid-1
+
         return -1
