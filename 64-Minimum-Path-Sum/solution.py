c20b828b69eda585796c06eeda7613f2cfaa9bf3
Victor <ychtan@email.gwu.edu>
2016-07-08 16:51:34 -0400

    [64. Minimum Path Sum][Accepted]committed by Victor

--- 64-Minimum-Path-Sum/solution.py
+++ 64-Minimum-Path-Sum/solution.py
@@ -8,25 +8,30 @@ class Solution(object):
         # dp[i,j]=min(dp[i-1,j],dp[i,j-1])+grid[i][j]
         # dfs make all things possible that's why the dfs TLE
         
+        # reduce the record space,
+        # we only need record the previous column and current column
         row=len(grid)
         col=len(grid[0])
         if row==0 or col==0:
             return 0
-
-        dp=[[0 for _ in xrange(col)] for _ in xrange(row)]
-        dp[0][0]=grid[0][0]
+        precol=[grid[0][0] for _ in xrange(row)]
+        curcol=[0 for _ in xrange(row)]
+        
         # deal the first row
         for i in xrange(1,row):
-            dp[i][0]=dp[i-1][0]+grid[i][0]
-        # deal the first column
+            precol[i]=precol[i-1]+grid[i][0]
+
         for j in xrange(1,col):
-            dp[0][j]=dp[0][j-1]+grid[0][j]
-
-        for i in xrange(1,row):
-            for j in xrange(1,col):
-                dp[i][j]=min(dp[i-1][j],dp[i][j-1])+grid[i][j]
-
-        return dp[row-1][col-1]
+            # current column is == precol+grid[0][j]
+            # precol[0] is updated by replace
+            curcol[0]=precol[0]+grid[0][j]
+            for i in xrange(1,row):
+                curcol[i]=min(curcol[i-1],precol[i])+grid[i][j]
+            # update the previous using current value and then move on
+            # since the curcol will update in the next loop
+            precol,curcol=curcol,precol
+            
+        return precol[row-1]
 
         
         
