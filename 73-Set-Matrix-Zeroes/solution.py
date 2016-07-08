7c7de9a4e05c35091c3dd79b502bc2e69d29c070
Victor <ychtan@email.gwu.edu>
2016-07-07 21:18:24 -0400

    [73. Set Matrix Zeroes][Accepted]committed by Victor

--- 73-Set-Matrix-Zeroes/solution.py
+++ 73-Set-Matrix-Zeroes/solution.py
@@ -1,3 +1,56 @@
-void setZeroes(int** matrix, int matrixRowSize, int matrixColSize) {
-    
-}
+class Solution(object):
+    def setZeroes(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: void Do not return anything, modify matrix in-place instead.
+        """
+        row=len(matrix)
+        col=len(matrix[0])
+        
+        if row==0:
+            return
+        # using the first row and first col to record the left matrix
+        
+        row_first=False
+        col_first=False
+        
+        # first colomn
+        for i in xrange(row):
+            if matrix[i][0]==0:
+                col_first=True
+                break
+        # fist row
+        for j in xrange(col):
+            if matrix[0][j]==0:
+                row_first=True
+                break
+        
+        # the left part
+        for i in xrange(1,row):
+            for j in xrange(1,col):
+                if matrix[i][j]==0:
+                    # using the first row to record
+                    matrix[i][0]=0
+                    matrix[0][j]=0
+        
+        # set all the other row to zero
+        for i in xrange(1,row):
+            if matrix[i][0]==0:
+                for j in xrange(1,col):
+                    matrix[i][j]=0
+        
+        # set all the other col to zero
+        for j in xrange(1,col):
+            if matrix[0][j]==0:
+                for i in xrange(1,row):
+                    matrix[i][j]=0
+        
+        # set the first row
+        if col_first:
+            for i in xrange(row):
+                matrix[i][0]=0
+        # set the first column
+        if row_first:
+            for j in xrange(col):
+                matrix[0][j]=0
+                
