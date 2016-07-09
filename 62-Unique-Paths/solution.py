db014af7a14bd4077935a616091a118c503fcc30
Victor <ychtan@email.gwu.edu>
2016-07-08 20:18:49 -0400

    [62. Unique Paths][Accepted]committed by Victor

--- 62-Unique-Paths/solution.py
+++ 62-Unique-Paths/solution.py
@@ -9,13 +9,11 @@ class Solution(object):
         # reduce space to record the previous and current row
         if m>n:
             return self.uniquePaths(n,m)
-            
-        precol=[1 for _ in xrange(m)]
+
         curcol=[1 for _ in xrange(n)]
         
         for j in xrange(1,n):
             for i in xrange(1,m):
-                curcol[i]=precol[i]+curcol[i-1]
-            precol,curcol=curcol,precol
+                curcol[i]=curcol[i]+curcol[i-1]
         
-        return precol[m-1]
+        return curcol[m-1]
