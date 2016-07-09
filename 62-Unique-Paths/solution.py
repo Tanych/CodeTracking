7f9aa79f49b340d89b6bd75b2f72690d21ad9a37
Victor <ychtan@email.gwu.edu>
2016-07-08 20:18:01 -0400

    [62. Unique Paths][Accepted]committed by Victor

--- 62-Unique-Paths/solution.py
+++ 62-Unique-Paths/solution.py
@@ -6,10 +6,16 @@ class Solution(object):
         :rtype: int
         """
         # it's very similar with the Minimum Path Sum #64
-        dp=[[1 for _ in xrange(n)] for _ in xrange(m)]
+        # reduce space to record the previous and current row
+        if m>n:
+            return self.uniquePaths(n,m)
+            
+        precol=[1 for _ in xrange(m)]
+        curcol=[1 for _ in xrange(n)]
         
-        for i in xrange(1,m):
-            for j in xrange(1,n):
-                dp[i][j]=dp[i-1][j]+dp[i][j-1]
+        for j in xrange(1,n):
+            for i in xrange(1,m):
+                curcol[i]=precol[i]+curcol[i-1]
+            precol,curcol=curcol,precol
         
-        return dp[m-1][n-1]
+        return precol[m-1]
