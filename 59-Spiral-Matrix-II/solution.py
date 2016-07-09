f578f2ca27e3ec2b4a4fdc7cca2f20b63fd6616c
Victor <ychtan@email.gwu.edu>
2016-07-09 10:34:10 -0400

    [59. Spiral Matrix II][Accepted]committed by Victor

--- 59-Spiral-Matrix-II/solution.py
+++ 59-Spiral-Matrix-II/solution.py
@@ -8,38 +8,32 @@ class Solution(object):
             return []
         matrix=[[0 for _ in xrange(n)] for _ in xrange(n)]
         
-        # using four value to record the boundary of the direction
-        left=0
-        up=0
-        down=n-1
-        right=n-1
-        # 0-move right,1 move down, 2 move left, 3 move up
-        direction=0 
+        # using i,j to record the pos
+        i=j=0
+        total=n*n
         start=1
-        while True:
-            if direction==0:
-                for i in xrange(left,right+1):
-                    matrix[up][i]=start
-                    start+=1
-                up+=1
-            if direction==1:
-                for i in xrange(up,down+1):
-                    matrix[i][right]=start
-                    start+=1
-                right-=1
-            if direction==2:
-                for i in xrange(right,left-1,-1):
-                    matrix[down][i]=start
-                    start+=1
-                down-=1
-            if direction==3:
-                for i in xrange(down,up-1,-1):
-                    matrix[i][left]=start
-                    start+=1
-                left+=1
-            
-            if up>down or left>right:
-                return matrix
-            # the next direction
-            direction=(direction+1)%4
-            
+        matrix[0][0]=1
+        while start<total:
+            while j+1<n and matrix[i][j+1]==0:
+                start+=1
+                matrix[i][j+1]=start
+                j+=1
+               
+            while i+1<n and matrix[i+1][j]==0:
+                start+=1
+                matrix[i+1][j]=start
+                i+=1
+                
+            while j-1>=0 and matrix[i][j-1]==0:
+                start+=1
+                matrix[i][j-1]=start
+                j-=1
+                
+            while i-1>=0 and matrix[i-1][j]==0:
+                start+=1
+                matrix[i-1][j]=start
+                i-=1
+               
+        
+        return matrix
+        
