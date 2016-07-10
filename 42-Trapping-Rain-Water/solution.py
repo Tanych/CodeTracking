88e6e2cdc4916c202b7936734304baa301113ff1
Victor <ychtan@email.gwu.edu>
2016-07-10 15:26:36 -0400

    [42. Trapping Rain Water][Accepted]committed by Victor

--- 42-Trapping-Rain-Water/solution.py
+++ 42-Trapping-Rain-Water/solution.py
@@ -6,17 +6,22 @@ class Solution(object):
         """
         n=len(height)
         left,right=0,n-1
+        # using to max to set the wall of each side
         leftmax=0
         rightmax=0
         total=0
         while left<=right:
+            # if the right wall is eough higher
             if height[left]<=height[right]:
+                # count the left part of the sunk down
                 if height[left]<leftmax:
                     total+=leftmax-height[left]
                 else:
+                    # the count a new higher wall, update
                     leftmax=height[left]
                 left+=1
             else:
+                # the same with left part
                 if height[right]<rightmax:
                     total+=rightmax-height[right]
                 else:
