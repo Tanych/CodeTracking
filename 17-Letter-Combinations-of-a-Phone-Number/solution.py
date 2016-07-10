56e55f771a863724c31fb06535e50ca4785bfc46
Victor <ychtan@email.gwu.edu>
2016-07-10 00:12:20 -0400

    [17. Letter Combinations of a Phone Number][Accepted]committed by Victor

--- 17-Letter-Combinations-of-a-Phone-Number/solution.py
+++ 17-Letter-Combinations-of-a-Phone-Number/solution.py
@@ -8,10 +8,15 @@ class Solution(object):
         if not digits:
             return []
         results = ['']
-        map = {'2':'abc', '3':'def', '4':'ghi', '5':'jkl', '6':'mno', '7':'pqrs', '8':'tuv', '9':'wxyz'}
+        mapping = {'0':'', '1':'', '2':'abc', '3':'def', '4':'ghi', '5':'jkl', '6':'mno', '7':'pqrs', '8':'tuv', '9':'wxyz'}
         
         for digit in digits:
-            results = [result+d for result in results for d in map[digit]]
+            candiate=mapping[digit]
+            tmp=[]
+            for ch in candiate:
+                for resch in results:
+                    tmp.append(resch+ch)
+            results,tmp=tmp,results
             
         return results
         
