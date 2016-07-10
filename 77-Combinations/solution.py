50feb1d1b5a2c98a69f9ead1e305ca114533c53e
Victor <ychtan@email.gwu.edu>
2016-07-10 10:41:22 -0400

    [77. Combinations][Accepted]committed by Victor

--- 77-Combinations/solution.py
+++ 77-Combinations/solution.py
@@ -5,20 +5,5 @@ class Solution(object):
         :type k: int
         :rtype: List[List[int]]
         """
-        digits=[]
-        for i in xrange(1,n+1):
-            digits.append(i)
-        
-        def dfs(nums,path,res,level,step):
-            print level,k
-            if step==k:
-                res.append(path)
-                return
-            
-            if step>k:
-                return
-            for i in xrange(level,len(nums)):
-                dfs(nums[:i]+nums[i+1:],path+[nums[i]],res,i,step+1)
-        res=[]
-        dfs(digits,[],res,0,0)
-        return res
+        from itertools import combinations
+        return list(combinations(range(1, n+1), k))
