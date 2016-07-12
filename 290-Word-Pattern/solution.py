336dd516f73cc853aca423ac5d198ad9b9adf7ca
Victor <ychtan@email.gwu.edu>
2016-07-12 17:14:51 -0400

    [290. Word Pattern][Accepted]committed by Victor

--- 290-Word-Pattern/solution.py
+++ 290-Word-Pattern/solution.py
@@ -11,21 +11,20 @@ class Solution(object):
         strlist=strs.split(" ")
         if len(strlist)!=len(pattern):
             return False
-        # building pattern map    
-        hashmap={}
-        for i in xrange(len(pattern)):
-            hashmap[pattern[i]]=hashmap.get(pattern[i],[])+[i]
-        
-        # check the intersection is equal
-        intersection=[]
-        for li in hashmap.values():
-            intersection.append(li[0])
-            # check in the same pattern
-            for i in xrange(1,len(li)):
-                if strlist[li[0]]!=strlist[li[i]]:
-                    return False
-        # if not in the same pattern , should not equal    
-        for i in xrange(1,len(intersection)):
-            if strlist[intersection[0]]==strlist[intersection[i]]:
+        # chars map
+        charmap=[None]*26
+        plist=list(pattern)
+        while len(plist):
+            string,ch=strlist.pop(),plist.pop()
+            # get the index
+            index=ord(ch)-97
+            
+            if charmap[index]!=string and charmap[index]:
                 return False
+            elif charmap[index]!=string and string in charmap:
+                return False
+            elif string not in charmap:
+                charmap[index]=string
         return True
+                
+                
