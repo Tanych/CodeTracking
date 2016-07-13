104312342cb1535f8f50be4ea6b811520abb7e77
Victor <ychtan@email.gwu.edu>
2016-07-12 21:57:01 -0400

    [187. Repeated DNA Sequences][Accepted]committed by Victor

--- 187-Repeated-DNA-Sequences/solution.py
+++ 187-Repeated-DNA-Sequences/solution.py
@@ -1,4 +1,10 @@
 class Solution(object):
+    def str2int(self,s):
+        res=0
+        for i in xrange(len(s)):
+            res=(res<<3)+(ord(s[i])&7)
+        return res
+    
     def findRepeatedDnaSequences(self, s):
         """
         :type s: str
@@ -10,7 +16,7 @@ class Solution(object):
         hashmap={}
         res=[]
         for i in xrange(len(s)-9):
-            hashmap[s[i:i+10]]=hashmap.get(s[i:i+10],0)+1
-            if hashmap[s[i:i+10]]==2:
+            hashmap[self.str2int(s[i:i+10])]=hashmap.get(self.str2int(s[i:i+10]),0)+1
+            if hashmap[self.str2int(s[i:i+10])]==2:
                 res.append(s[i:i+10])
         return res
