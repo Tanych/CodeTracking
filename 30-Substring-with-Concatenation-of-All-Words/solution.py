7f449306a7334247e9682bb9c6e294db7065fb05
Victor <ychtan@email.gwu.edu>
2016-07-13 17:01:58 -0400

    [30. Substring with Concatenation of All Words][Time Limit Exceeded]committed by Victor

--- 30-Substring-with-Concatenation-of-All-Words/solution.py
+++ 30-Substring-with-Concatenation-of-All-Words/solution.py
@@ -5,6 +5,7 @@ class Solution(object):
         :type words: List[str]
         :rtype: List[int]
         """
+        from copy import deepcopy
         if not words:
             return []
         word_len=len(words[0])
@@ -12,29 +13,27 @@ class Solution(object):
         # building map for words
         res=[]
         i=0
+        hashmap={}
+        for word in words:
+            hashmap[word]=hashmap.get(word,0)+1
+                
         while i+total_len<=len(s):
             j=i
-            found=True
-            hashmap={}
-            # reset the hashmap every trial
-            for word in words:
-                hashmap[word]=hashmap.get(word,0)+1
-                
+            copy=deepcopy(hashmap)
             while j< i+total_len:
                 # if not in the hash, break
-                if s[j:word_len+j] not in hashmap:
-                    found=False
+                if s[j:word_len+j] not in copy:
                     break
                 else:
                     # if find than check whether occurs before
-                    if hashmap[s[j:word_len+j]]<=0:
-                        found=False
-                        break
-                    # no, update the occurs
-                    hashmap[s[j:word_len+j]]-=1
+                    if copy[s[j:word_len+j]]==1:
+                        del copy[s[j:word_len+j]]
+                    else:
+                        # no, update the occurs
+                        copy[s[j:word_len+j]]-=1
                 j+=word_len
             # if all in hashmap and only occurs once, add to res
-            if found:
+            if len(copy)==0:
                 res.append(i)
             i+=1
         return res
