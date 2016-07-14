1340d0c21a143b1005fc86bc5f214128ee2587f9
Victor <ychtan@email.gwu.edu>
2016-07-13 21:02:19 -0400

    [49. Group Anagrams][Accepted]committed by Victor

--- 49-Group-Anagrams/solution.py
+++ 49-Group-Anagrams/solution.py
@@ -1,4 +1,12 @@
 class Solution(object):
+    def key(self, s):
+        INT_MAX=2<<31-1
+        result=1
+        for ch in s:
+            n=ord(ch)-ord('a')+1
+            result*=(n*n+n+41)%INT_MAX
+        return result
+        
     def groupAnagrams(self, strs):
         """
         :type strs: List[str]
@@ -8,7 +16,7 @@ class Solution(object):
         hashmap={}
         for i in xrange(n):
             # sorted the key 
-            key=''.join(sorted(list(strs[i])))
+            key=self.key(strs[i])
             # if the key is the same plus
             hashmap[key]=hashmap.get(key,[])+[strs[i]]
         return [v for v in hashmap.values()]
