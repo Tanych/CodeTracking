e2bc317b8f2ba300998bfc3c80270c18047c9f43
Victor <ychtan@email.gwu.edu>
2016-07-14 16:27:00 -0400

    [138. Copy List with Random Pointer][Accepted]committed by Victor

--- 138-Copy-List-with-Random-Pointer/solution.py
+++ 138-Copy-List-with-Random-Pointer/solution.py
@@ -6,28 +6,6 @@
 #         self.random = None
 
 class Solution(object):
-    def printList(self,head):
-        r=head
-        while r!=None:
-            print r.label,r.next,r.random
-            r=r.next
-
-    def searchNodeidx(self,head,node):
-        p=head
-        idx=0
-        while p!=None:
-            if node.label==p.label and node.next==p.next and node.random==p.random:
-                return idx
-            idx+=1
-            p=p.next
-
-    def searchNode(self,head,idx):
-        p=head
-        while idx>0:
-            p=p.next
-            idx-=1
-        return p
-
     def copyRandomList(self, head):
         """
         :type head: RandomListNode
@@ -36,32 +14,24 @@ class Solution(object):
         if not head:
             return None
             
+        # using hash map to store the value of the object
+        hashmap={}
+        hashmap[head]=RandomListNode(head.label)
         p=head
-        copyrdm=RandomListNode(head.label)
-        q=copyrdm
-        while p.next!=None:
-            # next copy
-            q.next=RandomListNode(p.next.label)
-            # move on for the next
-            q=q.next
+        while p:
+            if p.random:
+                # if the first time to create, build the node
+                if p.random not in hashmap:
+                    hashmap[p.random]=RandomListNode(p.random.label)
+                # if already create, directy pointer
+                hashmap[p].random=hashmap[p.random]
+            if p.next:
+                # if the first time to create, build the node
+                if p.next not in hashmap:
+                    hashmap[p.next]=RandomListNode(p.next.label)
+                # if already create, directy pointer
+                hashmap[p].next=hashmap[p.next]
+            # move on
             p=p.next
-
-        # deal with random
-        res=[]
-        p=head
-        while p!=None:
-            if not p.random:
-                res.append(-1)
-            else:
-                res.append(self.searchNodeidx(head,p.random))
-            p=p.next
-
-        # build the radom list
-        q=copyrdm
-        for i in xrange(len(res)):
-            if res[i]!=-1:
-                q.random=self.searchNode(copyrdm,res[i])
-            q=q.next
-            
-        return copyrdm
+        return hashmap[head]
         
