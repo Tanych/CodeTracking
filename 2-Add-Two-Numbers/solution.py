d7118940033a17ed64c329cb224c513328979e99
Victor <ychtan@email.gwu.edu>
2016-07-16 15:49:16 -0400

    [2. Add Two Numbers][Accepted]committed by Victor

--- 2-Add-Two-Numbers/solution.py
+++ 2-Add-Two-Numbers/solution.py
@@ -5,19 +5,6 @@
 #         self.next = None
 
 class Solution(object):
-    def reverselist(self,head):
-        if not head or not head.next:
-            return head
-        
-        pre=None
-        cur=head
-        while cur:
-            next=cur.next
-            cur.next=pre
-            pre=cur
-            cur=next
-        return pre
-        
     def addTwoNumbers(self, l1, l2):
         """
         :type l1: ListNode
@@ -35,6 +22,7 @@ class Solution(object):
         newhead=ListNode(-1)
         p=newhead
         
+        # plus the common length part
         while l1 and l2:
             sumval=l1.val+l2.val+carry
             tval=sumval%10
@@ -43,7 +31,9 @@ class Solution(object):
             p=p.next
             l1=l1.next
             l2=l2.next
-        
+            
+        # plus the l1 longer part
+        # be careful the carry contribute in last op
         while l1:
             if carry:
                 sumval=l1.val+carry
@@ -55,6 +45,8 @@ class Solution(object):
             p=p.next
             l1=l1.next
         
+        # plus the l2 longer part
+        # be careful the carry contribute in last op
         while l2:
             # make sure the previous carry
             if carry:
@@ -66,7 +58,8 @@ class Solution(object):
                 p.next=ListNode(l2.val)
             p=p.next
             l2=l2.next
-        # keep the last carry
+            
+        # if all finished but the last op has carry
         if carry:
             p.next=ListNode(carry)
         
