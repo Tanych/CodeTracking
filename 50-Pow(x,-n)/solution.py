65c6a21ac0b89b5583cee5af0e2803dfeda19946
Victor <ychtan@email.gwu.edu>
2016-07-17 10:18:50 -0400

    [50. Pow(x, n)][Accepted]committed by Victor

--- 50-Pow(x,-n)/solution.py
+++ 50-Pow(x,-n)/solution.py
@@ -10,11 +10,14 @@ class Solution(object):
             n=-n
             x=1.0/x
             
+        tb=[0.0]*32
+        tb[0]=x
+        
         res=1
-        while n>0:
-            if n&1:
-                res*=x
-            x*=x
-            n/=2
+        for i in xrange(1,32):
+            tb[i]=tb[i-1]*tb[i-1]
+        
+        for i in xrange(32):
+            if n&(1<<i):
+                res*=tb[i]
         return res
-            
