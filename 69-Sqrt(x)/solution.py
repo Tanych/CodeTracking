22bb5db94177b575eda2802ee5a31a2ddec4bcec
Victor <ychtan@email.gwu.edu>
2016-07-17 09:38:49 -0400

    [69. Sqrt(x)][Accepted]committed by Victor

---
+++ 69-Sqrt(x)/solution.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def mySqrt(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        if x==1:
+            return 1
+        
+        begin,end=1,x/2
+        while begin<end:
+            mid=(begin+end)/2
+            print mid
+            sqt=mid*mid
+            if x==sqt:
+                return mid
+            elif x>sqt:
+                begin=mid+1
+            else:
+                end=mid-1
+                
+        return begin-1 if x<begin*begin else begin
