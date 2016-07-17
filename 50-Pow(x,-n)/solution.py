fae7149e5817e44d3f875752c00fa46a88a7e486
Victor <ychtan@email.gwu.edu>
2016-07-17 10:08:45 -0400

    [50. Pow(x, n)][Accepted]committed by Victor

--- 50-Pow(x,-n)/solution.py
+++ 50-Pow(x,-n)/solution.py
@@ -5,14 +5,17 @@ class Solution(object):
         :type n: int
         :rtype: float
         """
+        if n<0:
+            return self.myPow(1.0/x,-n)
         if n==0:
             return 1
+        if n==1:
+            return x
+        if n==2:
+            return x*x
+        # odd
+        if not n%2:
+            return self.myPow(self.myPow(x,n/2),2)
+        if n%2:
+            return x*self.myPow(self.myPow(x,n/2),2)
             
-        res=x
-        num=n if n>=0 else -n
-        for i in xrange(num-1):
-            res*=x
-        if n<0:
-            res=1.0/res 
-
-        return res
