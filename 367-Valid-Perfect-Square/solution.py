061c261b7529c7e7dd2bb876d31a0b5bf0eb6514
Victor <ychtan@email.gwu.edu>
2016-07-17 09:33:05 -0400

    [367. Valid Perfect Square][Accepted]committed by Victor

--- 367-Valid-Perfect-Square/solution.py
+++ 367-Valid-Perfect-Square/solution.py
@@ -7,8 +7,26 @@ class Solution(object):
         # the newton interge
         # https://en.wikipedia.org/wiki/Integer_square_root#Using_only_integer_division
         # it's short but not the cs way
-        i=num
-        while i*i>num:
-            print i
-            i=(i+num/i)/2
-        return i*i==num
+        # i=num
+        # while i*i>num:
+        #     print i
+        #     i=(i+num/i)/2
+        # return i*i==num
+        
+        #in cs binary search
+        # using two pointer to search betweent[1,num/2+1]
+        
+        begin,end=1,num/2+1
+        while begin<end:
+            mid=(begin+end)/2
+            print mid
+            sqt=mid*mid
+            if num==sqt:
+                return True
+            elif num>sqt:
+                begin=mid+1
+            else:
+                end=mid-1
+                
+        return begin*begin==num
+        
