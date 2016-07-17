6dc2e053f6ac0ae19ae0716721c8fbf3e3205897
Victor <ychtan@email.gwu.edu>
2016-07-16 23:23:10 -0400

    [368. Largest Divisible Subset][Accepted]committed by Victor

--- 368-Largest-Divisible-Subset/solution.py
+++ 368-Largest-Divisible-Subset/solution.py
@@ -9,6 +9,14 @@ class Solution(object):
             # set combine
             # if num is the mutip of key then num is the mutip of all the ele
             # in the map
-            hashmap[num]=max((hashmap[key] for key in hashmap if num%key==0),key=len)|{num}
+            maxdidlen=0
+            maxkey=-1
+            for key in hashmap:
+                if num%key==0:
+                    if len(hashmap[key])>maxdidlen:
+                        maxdidlen=len(hashmap[key])
+                        maxkey=key
+            hashmap[num]=hashmap[maxkey]|{num}
+            #hashmap[num]=max((hashmap[key] for key in hashmap if num%key==0),key=len)|{num}
         #print hashmap
         return list(max(hashmap.values(),key=len))
