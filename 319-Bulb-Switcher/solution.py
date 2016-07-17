6749e106a9223018ab4c03061df61984dddbca68
Victor <ychtan@email.gwu.edu>
2016-07-17 15:07:04 -0400

    [319. Bulb Switcher][Accepted]committed by Victor

--- 319-Bulb-Switcher/solution.py
+++ 319-Bulb-Switcher/solution.py
@@ -8,18 +8,4 @@ class Solution(object):
             return 0
         if n==1:
             return 1
-        rounds=2
-        
-        res=[1]*n
-        
-        while rounds<=n:
-            for i in xrange(rounds-1,n):
-                if (i+1)%rounds==0:
-                    res[i]^=1
-            rounds+=1
-        #print res
-        cnt=0
-        for i in xrange(n):
-            if res[i]==1:
-                cnt+=1
-        return cnt
+        return int(math.sqrt(n))
