45f7d784e6bbcdb2c5dc0638ed4796f7161e839d
Victor <ychtan@email.gwu.edu>
2016-07-17 16:33:36 -0400

    [171. Excel Sheet Column Number][Accepted]committed by Victor

--- 171-Excel-Sheet-Column-Number/solution.py
+++ 171-Excel-Sheet-Column-Number/solution.py
@@ -1,4 +1,16 @@
 class Solution(object):
+    def myPow(self, x, n):
+        if n<0:
+            n=-n
+            x=1.0/x
+        res=1
+        while n>0:
+            if n&1:
+                res*=x
+            x*=x
+            n/=2
+        return res
+        
     def titleToNumber(self, s):
         """
         :type s: str
@@ -7,6 +19,6 @@ class Solution(object):
         n=len(s)
         sum=0
         for i in xrange(n):
-            sum+=int((ord(s[i])-ord('A')+1)*math.pow(26,n-i-1))
+            sum+=int((ord(s[i])-ord('A')+1)*self.myPow(26,n-i-1))
         
         return sum
