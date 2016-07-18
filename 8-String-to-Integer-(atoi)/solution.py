52fc9fb8aeacf29730bebe16157d231e2de905f6
Victor <ychtan@email.gwu.edu>
2016-07-17 22:55:48 -0400

    [8. String to Integer (atoi)][Accepted]committed by Victor

--- 8-String-to-Integer-(atoi)/solution.py
+++ 8-String-to-Integer-(atoi)/solution.py
@@ -7,15 +7,13 @@ class Solution(object):
         n=len(strs)
         if n==0:
             return 0
-        
-        int_res=[]
+            
         start=0
         neg=1
-        for i in xrange(n):
-            if strs[i]==" ":
-               start+=1
-            else:
-                break
+        
+        while start<n and strs[start]==" ":
+            start+=1
+            
         # get rid of str
         if len(strs)==start or (strs[start]!='-' and strs[start]!='+' and not strs[start].isdigit()):
             return 0
@@ -28,7 +26,7 @@ class Solution(object):
             start+=1
 
         sum_int=0
-        while start<len(strs) and strs[start].isdigit():
+        while start<n and strs[start].isdigit():
             digit=ord(strs[start])-ord('1')+1
             sum_int=sum_int*10+digit
             start+=1
