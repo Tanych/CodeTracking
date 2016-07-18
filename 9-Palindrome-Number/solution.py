ea8a4db6e87da14627669bc2cf27076b417c1211
Victor <ychtan@email.gwu.edu>
2016-07-17 23:22:13 -0400

    [9. Palindrome Number][Accepted]committed by Victor

---
+++ 9-Palindrome-Number/solution.py
@@ -0,0 +1,40 @@
+class Solution(object):
+    def reverseint(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        negflag=1
+        in_x=x
+        if x<0:
+            negflag=-1
+            in_x=-x
+        if in_x==0:
+            return 0
+            
+        if in_x<10:
+            return in_x*negflag
+        sum_int=0  
+        while in_x/10:
+            sum_int=sum_int*10+ in_x%10
+            in_x/=10
+        sum_int=sum_int*10+in_x
+        
+        return negflag*sum_int if sum_int<((1<<31)-1) else -1
+        
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        if x<0:
+            return False
+            
+        if x>(1<<31)-1:
+            return False
+            
+        if x>=0 and x<10:
+            return True
+            
+        return x==self.reverseint(x)
+        
