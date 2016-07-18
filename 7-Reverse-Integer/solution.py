b5ba0a8aa5076f0a0ac6c7212bbed92f2925783b
Victor <ychtan@email.gwu.edu>
2016-07-17 23:08:35 -0400

    [7. Reverse Integer][Accepted]committed by Victor

--- 7-Reverse-Integer/solution.py
+++ 7-Reverse-Integer/solution.py
@@ -14,27 +14,12 @@ class Solution(object):
             
         if in_x<10:
             return in_x*negflag
-            
-        int_res=[]
-        while in_x:
-            digit=in_x%10
+        sum_int=0  
+        while in_x/10:
+            sum_int=sum_int*10+ in_x%10
             in_x/=10
-            int_res.append(digit)
-        #print int_res
-        sum_int=0
-        start=0
-        n=len(int_res)
-        # get rid of the zeros
-        for i in xrange(len(int_res)):
-            if int_res[i]==0:
-                start+=1
-            else:
-                break
-            
-        res=int_res[start:n]
-        for j in xrange(len(res)):
-            sum_int+=int(res[j]*math.pow(10,len(res)-j-1))
-        # judge the overflow
+        sum_int=sum_int*10+in_x
+        
         return negflag*sum_int if sum_int<((1<<31)-1) else 0
         
             
