ba803dffdc54dcd9454450f2594ab4cba1f0216c
Victor <ychtan@email.gwu.edu>
2016-07-19 23:13:19 -0400

    [344. Reverse String][Time Limit Exceeded]committed by Victor

---
+++ 344-Reverse-String/solution.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def reverseString(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        if not s:
+            return ""
+        
+        left,right=0,len(s)-1
+        
+        lis=list(s)
+        
+        while left<right:
+            lis[left],lis[right]=lis[right],lis[left]
+        
+        return ''.join(lis)
