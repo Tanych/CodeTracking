c97ae299e6308798fce8a54aae4acb05abef9daa
Victor <ychtan@email.gwu.edu>
2016-07-19 22:48:53 -0400

    [273. Integer to English Words][Accepted]committed by Victor

--- 273-Integer-to-English-Words/solution.py
+++ 273-Integer-to-English-Words/solution.py
@@ -9,56 +9,26 @@ class Solution(object):
             
         mapping=["","Thousand","Million", "Billion"]
 
-        res=[0,0,0,0]
-        i=0
-        while num>0:
-            res[i]=num%1000
-            num=num/1000
-            i+=1
-
         resstr=''
-        for i in xrange(len(res)-1,-1,-1):
-            if res[i]:
-                resstr+=self.helperths(res[i])
-                if mapping[i]:
-                    resstr+=' '+mapping[i]+' '
+        for i in range(len(mapping)):
+            if num%1000 != 0:
+                resstr = self.helperths(num%1000) + mapping[i] + ' ' + resstr
+            num /= 1000
         return resstr.strip()
 
     def helperths(self,num):
         """
         deal with the number less than one thousand
         """
-        mapping={0:'Zero',1:'One',2:'Two',3:'Three',4:'Four',5:'Five',6:'Six',7:'Seven',8:'Eight',9:'Nine',10:'Ten',
-                 11:'Eleven',12:"Twelve",13:"Thirteen",14:"Fourteen",15:"Fifteen",16:"Sixteen",17:"Seventeen",18:"Eighteen",19:"Nineteen",20:"Twenty",
-                 30:"Thirty",40:"Forty",50:"Fifty",60:"Sixty",70:"Seventy",80:"Eighty",90:"Ninety"}
-
+        lesstw = ["","One","Two","Three","Four","Five","Six","Seven","Eight","Nine","Ten","Eleven","Twelve","Thirteen","Fourteen","Fifteen","Sixteen","Seventeen","Eighteen","Nineteen"]
+        tens = ["","Ten","Twenty","Thirty","Forty","Fifty","Sixty","Seventy","Eighty","Ninety"]
+        
         if num==0:
-            return mapping[0]
-
-        res=[0,0,0]
-        i=0
-        while num:
-            digit=num%10
-            num=num/10
-            res[i]=digit
-            i+=1
-
-        resstr=''
-        leftstr=''
-        if res[-1]:
-            resstr+=mapping[res[-1]]+' Hundred'
-            
-        tenvalue=res[-2]*10+res[-3]
-        if 0<tenvalue<=20:
-            leftstr+=mapping[res[-2]*10+res[-3]]
-        elif tenvalue>20:
-            leftstr+=mapping[res[-2]*10]
-            if res[-3]:
-                leftstr+=' '+mapping[res[-3]]
-                
-        if resstr and leftstr:
-            return resstr+' '+leftstr
-        elif resstr:
-            return resstr
-        elif leftstr:
-            return leftstr
+            return ""
+        if 0<num<20:
+            return lesstw[num]+' '
+        if 20<=num<100:
+            return tens[num/10]+' '+self.helperths(num%10)
+        if num>=100:
+            return lesstw[num/100]+' Hundred '+self.helperths(num%100)
+       
