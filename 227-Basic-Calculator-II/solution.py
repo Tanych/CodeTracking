05677f30d9c237c153bd31eec020aaa54fc89cee
Victor <ychtan@email.gwu.edu>
2016-07-20 16:48:06 -0400

    [227. Basic Calculator II][Accepted]committed by Victor

--- 227-Basic-Calculator-II/solution.py
+++ 227-Basic-Calculator-II/solution.py
@@ -15,28 +15,27 @@ class Solution(object):
         """
         if not s:
             return 0
-        # the main idea is store the */ result temp
-        # and directly add the +- reuls to final result
-        # 1(+) -1(-) 2(*) 3(/)
-        resnum,num,op=0,0,1
-        # to store the temp result product of '/','*'
-        product=0
+        # using stack to store the result
+        numstk=[]
+        resnum=0
+        num=0
+        # 1(+),-1(-),2(*),3(/)
+        # should start with positive num
+        op=1
+        # inital the product with 1
+        product=1
+        
         for i in xrange(len(s)):
             if s[i].isdigit():
                 num=num*10+int(s[i])
-            elif s[i]=='+' or s[i]=='-':
-                # if the previous is '+' or '/'
-                # the result of '+' and '/' can be directyly add to the res
-                resnum+=product*num if op==2 else self.divideNeg(product,num) if op==3 else op*num
+            if s[i]=='+' or s[i]=='-':
+                numstk.append(product*num if op==2 else self.divideNeg(product,num) if op==3 else op*num)
                 op=1 if s[i]=='+' else -1
                 num=0
             elif s[i]=='*' or s[i]=='/':
-                # if previous is '*' or '/', to record the result temp
-                # to get the result of all the '*' '/'
                 product=product*num if op==2 else self.divideNeg(product,num) if op==3 else op*num
                 op=2 if s[i]=='*' else 3
                 num=0
-        # to deal with the last num and previous result
-        resnum+=product*num if op==2 else self.divideNeg(product,num) if op==3 else op*num
-        return resnum
+        numstk.append(product*num if op==2 else self.divideNeg(product,num) if op==3 else op*num)
+        return sum(numstk)
         
