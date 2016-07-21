785394f953620d1471097f13b17234e50219dd84
Victor <ychtan@email.gwu.edu>
2016-07-20 22:22:18 -0400

    [115. Distinct Subsequences][Accepted]committed by Victor

--- 115-Distinct-Subsequences/solution.py
+++ 115-Distinct-Subsequences/solution.py
@@ -20,6 +20,9 @@ class Solution(object):
         # 2. AE vs AECEE, if the E doesn't comes from the last 'E'
         # we need to find how many AE in AECE
         # dp[i][j]=dp[i][j-1]
+        
+        """
+        O(mn)--O(mn)
         tlen=len(t)
         slen=len(s)
         dp=[[0 for _ in xrange(slen+1)] for _ in xrange(tlen+1)]
@@ -35,5 +38,24 @@ class Solution(object):
                     dp[i][j]=dp[i][j-1]+(dp[i-1][j-1] if t[i-1]==s[j-1] else 0)
                     
         return dp[tlen][slen]
+        """
         
-        
+        # reduce the space to O(n)
+        # since it only the previous result
+        tlen=len(t)
+        slen=len(s)
+        dp=[0 for _ in xrange(tlen+1)]
+        dp[0]=1
+        for i in xrange(1,slen+1):
+            pre=dp[0]
+            for j in xrange(1,tlen+1):
+                # record the value before change 
+                # it means dp[i-1][j]
+                tmp=dp[j]
+                # update the dp[i][j]
+                # pre corresponds to dp[i-1][j-1]
+                dp[j]=tmp+(pre if t[j-1]==s[i-1] else 0)
+                pre=tmp
+                
+        return dp[-1]
+                
