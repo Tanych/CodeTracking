08fd91126a07cb8b21373f18fbb45e8c8882a783
Victor <ychtan@email.gwu.edu>
2016-07-21 14:43:35 -0400

    [97. Interleaving String][Accepted]committed by Victor

--- 97-Interleaving-String/solution.py
+++ 97-Interleaving-String/solution.py
@@ -1,5 +1,8 @@
 class Solution(object):
-    def helper(self,s1,s2,s3):
+    def helper(self,s1,s2,s3,hasvisited):
+        if (s1,s2,s3) in hasvisited:
+            return hasvisited[(s1,s2,s3)]
+            
         if not s1 and not s2 and not s3:
             return True
         
@@ -7,15 +10,14 @@ class Solution(object):
             return s2==s3
         if not s2:
             return s1==s3
-            
-        if s1[0]==s3[0] and s2[0]==s3[0]:
-            return self.helper(s1[1:],s2,s3[1:])|self.helper(s1,s2[1:],s3[1:])
-        elif s2[0]==s3[0]:
-            return self.helper(s1,s2[1:],s3[1:])
-        elif s1[0]==s3[0]:
-            return self.helper(s1[1:],s2,s3[1:])
-    
-        return False
+        s2res=s1res=False
+        if s2[0]==s3[0]:
+            s2res= self.helper(s1,s2[1:],s3[1:],hasvisited)
+        if s1[0]==s3[0]:
+            s1res= self.helper(s1[1:],s2,s3[1:],hasvisited)
+        
+        hasvisited[(s1,s2,s3)]=s1res|s2res
+        return hasvisited[(s1,s2,s3)]
         
                 
     def isInterleave(self, s1, s2, s3):
@@ -25,5 +27,6 @@ class Solution(object):
         :type s3: str
         :rtype: bool
         """
-        return self.helper(s1,s2,s3)
+        hasvisited={}
+        return self.helper(s1,s2,s3,hasvisited)
         
