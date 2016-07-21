7212765dcb18014058ea6f2ad8b637376baf2ef3
Victor <ychtan@email.gwu.edu>
2016-07-21 19:39:05 -0400

    [91. Decode Ways][Accepted]committed by Victor

--- 91-Decode-Ways/solution.py
+++ 91-Decode-Ways/solution.py
@@ -1,28 +1,39 @@
 class Solution(object):
-    def helper(self,s,mapping,hashvalue):
+    def __init__(self):
+        self.mapping={}
+        # inital the mapping values
+        for i in xrange(26):
+            self.mapping[str(i+1)]=chr(65+i)
+            
+        self.hashvalue={}
+            
+    def helper(self,s):
         
-        if s in hashvalue:
-            return hashvalue[s]
-            
-        if s[0] not in mapping:
+        # save time cost space
+        if s in self.hashvalue:
+            return self.hashvalue[s]
+        
+        # if the start not in mapping return 0
+        if s[0] not in self.mapping:
             return 0
-            
-        if len(s)==1 and s[0] in mapping:
+        
+        # if it's the last l
+        if len(s)==1 and s[0] in self.mapping:
             return  1
             
-        if len(s)==2:
-            if s in mapping and s[0] in mapping and s[1] in mapping:
-                return 2
-            if s in mapping:
-                return 1
+        # two elements if two situations
+        if len(s)==2 and s in self.mapping:
+            return 2 if s[0] in self.mapping and s[1] in self.mapping else 1
                 
         left=right=0
-        if s and s[0] in mapping:
-            left=self.helper(s[1:],mapping,hashvalue)
-        if s and s[0:2] in mapping:
-            right=self.helper(s[2:],mapping,hashvalue)
-        hashvalue[s]=left+right
-        return hashvalue[s]
+        if s and s[0] in self.mapping:
+            left=self.helper(s[1:])
+        if s and s[0:2] in self.mapping:
+            right=self.helper(s[2:])
+        # saving the value
+        self.hashvalue[s]=left+right
+        
+        return self.hashvalue[s]
         
     def numDecodings(self, s):
         """
@@ -32,11 +43,5 @@ class Solution(object):
         if not s:
             return 0
             
-        mapping={}
-        # inital the mapping values
-        for i in xrange(26):
-            mapping[str(i+1)]=chr(65+i)
-        
-        hashvalue={}
-        return self.helper(s,mapping,hashvalue)
+        return self.helper(s)
         
