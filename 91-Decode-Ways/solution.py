a8f03f3257b7ac2a8128a2e793e52da517c7a6af
Victor <ychtan@email.gwu.edu>
2016-07-21 19:30:42 -0400

    [91. Decode Ways][Time Limit Exceeded]committed by Victor

---
+++ 91-Decode-Ways/solution.py
@@ -0,0 +1,37 @@
+class Solution(object):
+    def helper(self,s,mapping):
+        if s[0] not in mapping:
+            return 0
+            
+        if len(s)==1 and s[0] in mapping:
+            return  1
+            
+        if len(s)==2:
+            if s in mapping and s[0] in mapping and s[1] in mapping:
+                return 2
+            if s in mapping:
+                return 1
+                
+        left=right=0
+        if s and s[0] in mapping:
+            left=self.helper(s[1:],mapping)
+        if s and s[0:2] in mapping:
+            right=self.helper(s[2:],mapping)
+        return left+right
+        
+    def numDecodings(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        if not s:
+            return 0
+            
+        mapping={}
+        # inital the mapping values
+        for i in xrange(26):
+            mapping[str(i+1)]=chr(65+i)
+        
+        
+        return self.helper(s,mapping)
+        
