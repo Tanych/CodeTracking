e252a08aa3cab511205599da059e4aa6f7164ce1
Victor <ychtan@email.gwu.edu>
2016-07-21 14:55:05 -0400

    [97. Interleaving String][Accepted]committed by Victor

--- 97-Interleaving-String/solution.py
+++ 97-Interleaving-String/solution.py
@@ -10,6 +10,10 @@ class Solution(object):
             return s2==s3
         if not s2:
             return s1==s3
+        # using the hashmap to record the same(s1,s2,s3) to reduce same count
+        # EX: if start with s1, the s1 becomes to reduce len1, s2 reduce to len2
+        # if these not exist, then rollback to recount start s2, s2 reduct to len2
+        # s1 reduce to s1, this time it's no need to count the again
         s2res=s1res=False
         if s2[0]==s3[0]:
             s2res= self.helper(s1,s2[1:],s3[1:],hasvisited)
