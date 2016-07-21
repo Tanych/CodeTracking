2418aa69226558c4b5f8bed52c3659e0a6e63916
Victor <ychtan@email.gwu.edu>
2016-07-20 23:07:22 -0400

    [93. Restore IP Addresses][Accepted]committed by Victor

---
+++ 93-Restore-IP-Addresses/solution.py
@@ -0,0 +1,43 @@
+class Solution(object):
+    def checknum(self,s):
+        if not s:
+            return False
+            
+        intnum=int(s)
+        lens=len(s)
+        if intnum>=0 and intnum<=9 and lens>1:
+            return False
+        if intnum>=10 and intnum<=99 and lens>2:
+            return False
+        if intnum>=100 and intnum<=255 and lens>3:
+            return False
+        return True
+        
+    def helper(self,s,step,ipstr,res):
+        # step means the level to recur
+        # only 4 level
+        if step==3:
+            # if reaches the final level
+            if self.checknum(s) and 0<=int(s)<=255:
+                ipstr+='.'+s
+                res.append(ipstr)
+                return 
+        # else continue to recusive find the result
+        for i in xrange(len(s)):
+            # try all possible value in 0-255
+            intnum=int(s[:i+1])
+            if not self.checknum(s[:i+1]):
+                continue
+            if 0<=intnum<=255:
+                self.helper(s[i+1:],step+1,ipstr+('.' if ipstr else '')+s[:i+1],res)
+        
+    def restoreIpAddresses(self, s):
+        """
+        :type s: str
+        :rtype: List[str]
+        """
+        if len(s)<4 or len(s)>12:
+            return []
+        res=[]
+        self.helper(s,0,"",res)
+        return res
