f0b3d50788d31c1fad48210086d2e9f297e4c1c6
Victor <ychtan@email.gwu.edu>
2016-07-21 16:04:54 -0400

    [97. Interleaving String][Accepted]committed by Victor

--- 97-Interleaving-String/solution.py
+++ 97-Interleaving-String/solution.py
@@ -1,4 +1,51 @@
 class Solution(object):
+    def isInterleaveDP(self,s1, s2, s3):
+        """
+        O(m*n) space 
+        """
+        len1,len2,len3=len(s1),len(s2),len(s3)
+        if len1+len2!=len3:
+            return False
+        # define: dp[i][j] means the s1(0,i) and s2(0,j) interveal in s3(0,i+j)
+        dp=[[True for _ in xrange(len2+1)] for _ in xrange(len1+1)]
+        
+        # initial value, only check s1, s3,suppose s2 is empty
+        for i in xrange(1,len1+1):
+            dp[i][0]=dp[i-1][0] and s1[i-1]==s3[i-1]
+        # only check s2 s3,suppose s1 is empty
+        for j in xrange(1,len2+1):
+            dp[0][j]=dp[0][j-1] and s2[j-1]==s3[j-1]
+        for i in xrange(1,len1+1):
+            for j in xrange(1,len2+1):
+                dp[i][j]=(dp[i-1][j] and s1[i-1]==s3[i-1+j]) or (dp[i][j-1] and s2[j-1]==s3[i+j-1])
+        return dp[-1][-1]
+        
+    def isInterleaveDP2(self,s1, s2, s3):
+        """
+        O(n) space 
+        """
+        len1,len2,len3=len(s1),len(s2),len(s3)
+        if len1+len2!=len3:
+            return False
+        # define: dp[i][j] means the s1(0,i) and s2(0,j) interveal in s3(0,i+j)
+        dp=[True for _ in xrange(len2+1)]
+        
+        # initial set up the table of 
+        for j in xrange(1,len2+1):
+            dp[j]=dp[j-1] and (s2[j-1]==s3[j-1])
+        
+        # we can image it only can move right and down in the matrix
+        for i in xrange(1,len1+1):
+            # inital the row value, since we only need the value in the previous column
+            dp[0]=dp[0] and s1[i-1]==s3[i-1]
+            for j in xrange(1,len2+1):
+                # the dp[j] in left = is the new row cossrespond to [i][j]
+                # the dp[j] in right = is the previous row value, [i-1][j-1]
+                # dp[j-1] is the previous result 
+                dp[j]=(dp[j] and s1[i-1]==s3[i-1+j]) or (dp[j-1] and s2[j-1]==s3[i+j-1])
+                
+        return dp[-1]
+        
     def helper(self,s1,s2,s3,hasvisited):
         if (s1,s2,s3) in hasvisited:
             return hasvisited[(s1,s2,s3)]
@@ -31,6 +78,19 @@ class Solution(object):
         :type s3: str
         :rtype: bool
         """
+        """
+        <Longest Test case>
+        "abbbbbbcabbacaacccababaabcccabcacbcaabbbacccaaaaaababbbacbb",
+        "ccaacabbacaccacababbbbabbcacccacccccaabaababacbbacabbbbabc",
+        "cacbabbacbbbabcbaacbbaccacaacaacccabababbbababcccbabcabbaccabcccacccaabbcbcaccccaaaaabaaaaababbbbacbbabacbbacabbbbabc"
+        """
+        """
+        # 1.recusive
         hasvisited={}
         return self.helper(s1,s2,s3,hasvisited)
+        """
+        # 2.dp with O(m*n)
+        #return self.isInterleaveDP(s1,s2,s3)
+        # 3.dp with O(n)
+        return self.isInterleaveDP2(s1,s2,s3)
         
