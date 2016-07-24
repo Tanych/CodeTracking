13874d1540465658e397978a8b7957deb904a6f3
Victor <ychtan@email.gwu.edu>
2016-07-24 14:48:27 -0400

    [338. Counting Bits][Accepted]committed by Victor

--- 338-Counting-Bits/solution.py
+++ 338-Counting-Bits/solution.py
@@ -1,17 +1,22 @@
 class Solution(object):
-    def countone(self,s):
-        count=0
-        for ch in s:
-            if ch=='1':
-                count+=1
-        return count
         
     def countBits(self, num):
         """
         :type num: int
         :rtype: List[int]
         """
-        res=[]
-        for i in xrange(num+1):
-            res.append(self.countone(bin(i)[2:]))
-        return res
+        """
+        do it as a dp
+        define: dp[i] means the ith number if 1count
+        dp[0]=0
+        """
+        dp=[0 for _ in xrange(num+1)]
+        for i in xrange(1,num+1):
+            # if i is odd, the could the half+1
+            # if i is even it equal the half
+            # we can image than <<1 is double 
+            # if the last digit is 0 we doulbe nothing
+            # if the last digit is 1 we lose 1
+            dp[i]=dp[i>>1]+1 if i&1 else dp[i>>1]
+            
+        return dp
