f5201dde01ade5c4bc944d64c7da4521d822ec02
Victor <ychtan@email.gwu.edu>
2016-07-24 19:27:53 -0400

    [221. Maximal Square][Accepted]committed by Victor

--- 221-Maximal-Square/solution.py
+++ 221-Maximal-Square/solution.py
@@ -1,9 +1,41 @@
 class Solution(object):
+    def maximalSquareON(self, matrix):
+        """
+        :type matrix: List[List[str]]
+        :rtype: int
+        """
+        if not matrix or not matrix[0]:
+            return 0
+        
+        row=len(matrix)
+        col=len(matrix[0])
+        
+        cur=[0 for _ in xrange(row+1)]
+        
+        maxw=pre=0
+        for j in xrange(1,col+1):
+            for i in xrange(1,row+1):
+                tmp=cur[i]
+                if matrix[i-1][j-1]=='1':
+                    # first choose the smller square between the up and left
+                    # it determin the length that [i-1][j-1] can be extend
+                    # finally, the square in the [i-2][i-2] also determin 
+                    # the dignoal line can be extend
+                    cur[i]=min(cur[i-1],cur[i],pre)+1
+                    maxw=max(maxw,cur[i])
+                else:
+                    cur[i]=0
+                pre=tmp
+                
+        return maxw*maxw
+        
     def maximalSquare(self, matrix):
         """
         :type matrix: List[List[str]]
         :rtype: int
         """
+        return self.maximalSquareON(matrix)
+        
         if not matrix or not matrix[0]:
             return 0
         
