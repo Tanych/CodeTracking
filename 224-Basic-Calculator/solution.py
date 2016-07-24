44a743708e20514600d2fe94daf6042f193c496f
Victor <ychtan@email.gwu.edu>
2016-07-23 20:07:21 -0400

    [224. Basic Calculator][Accepted]committed by Victor

--- 224-Basic-Calculator/solution.py
+++ 224-Basic-Calculator/solution.py
@@ -1,53 +1,77 @@
 class Solution(object):
-    def calculate(self, s):
+    def divideNeg(self,a,b):
+        """
+        python divide -3/2==-2  
+        """
+        if a*b<0:
+            return -(abs(a)/abs(b))
+        else:
+            return a/b
+            
+    def calcWithMD(self, s):
         """
         :type s: str
         :rtype: int
         """
         if not s:
             return 0
-            
-        resnum=0
-        #flag of while it's positive 1,-1
-        op=1
+        # using stack to store the result
+        numstk=[]
         num=0
-        stack_ex=[]
+        # 1(+),-1(-),2(*),3(/)
+        # should start with positive num
+        op='+'
         
         for i in xrange(len(s)):
             if s[i].isdigit():
                 num=num*10+int(s[i])
-                continue
+            if (not s[i].isdigit() and s[i]!=' ') or i==len(s)-1:
+                # the op is the previous op
+                # 14*3/2 whien s[i]=='*' ,op is the first '+'
+                if op=='+' or op=='-':
+                    numstk.append(num if op=='+' else -num)
+                elif op=='*' or op=='/':
+                    # get the previous op
+                    opval=numstk.pop()
+                    # push the result
+                    numstk.append(opval*num if op=='*' else self.divideNeg(opval,num))
+                # update op
+                op=s[i]
+                num=0
+        return sum(numstk) if numstk else 0
+
+    def calculate(self, s):
+        n=len(s)
+        if not s:
+            return -1
+
+        stck=[]
+        i=0
+        while i<n:
+            if s[i]==')':
+                strop=''
+                while stck and stck[-1]!='(':
+                    strop+=stck.pop()
+                value=self.calcWithMD(strop[::-1])
+                if stck and stck[-1]=='(':
+                    stck.pop()
+                # deal with mutil --
+                if stck and stck[-1]=='-':
+                    if value<0:
+                        stck.pop()
+                        stck.append('+')
+                        value=-value
+                # insert the reverse value since, pop
+                # will get the reverse string
+                stck.append(str(value)[::-1])
+            else:
+                stck.append(s[i])
+            i+=1
             
-            # save the num
-            stack_ex.append(op*num)
-            
-            # change the op value 
-            if s[i]=='+':
-                op=1
-            elif s[i]=='-':
-                op=-1
-            elif s[i]=='(':
-                stack_ex.append('+' if op==1 else '-')
-                # need to reset to "+" since "-(1" the first
-                # number of '(' should be positive
-                op=1
-            elif s[i]==')':
-                # begin to calc,+,- is the sign to stop add
-                # the number inside () is separte by + or -
-                parentSum=0
-                while stack_ex[-1]!='+' and stack_ex[-1]!='-':
-                    parentSum+=stack_ex.pop()
-                # if the op before '(' is '-' we need add to neg
-                if stack_ex.pop()=='-':
-                    parentSum=-parentSum
-                # update the sum of num in () to the stack
-                stack_ex.append(parentSum)
-            # reset the num
-            num=0
-        
-        # deal with the last num
-        if num:
-            stack_ex.append(op*num)
-            
-        return sum(stack_ex)
+        # calulate the last op without ()
+        strop=''
+        while stck:
+            strop+=stck.pop()
+        value=self.calcWithMD(strop[::-1])
+        return value
             
