20cb16af2e3aceafbc9d2dcf956ba76d26aa397c
Victor <ychtan@email.gwu.edu>
2016-07-27 23:29:50 -0400

    [131. Palindrome Partitioning][Accepted]committed by Victor

--- 131-Palindrome-Partitioning/solution.py
+++ 131-Palindrome-Partitioning/solution.py
@@ -1,15 +1,39 @@
 class Solution(object):
+    def dfshelper(self,res,dp,path,pos,strs):
+        # if reach the end of one path
+        n=len(strs)
+        if pos==n:
+            res.append(path)
+            return
+        
+        for i in xrange(pos,n):
+            # if currecnt pos is pandirome
+            if dp[pos][i]:
+                # add to path check the rest
+                self.dfshelper(res,dp,path+[strs[pos:i+1]],i+1,strs)
+        
+        
     def partition(self, s):
         """
         :type s: str
         :rtype: List[List[str]]
         """
-        res=[]
+        """
+        the python way is not gernal,
+        like do it like java and c++
+        using dp and backtracking
+        """
+        
+        # the dp is record substring[i,j] is a palindrome
         n=len(s)
-        for i in xrange(1,n+1):
-            # if s[:i] is palindrome
-            if s[:i]==s[i-1::-1]:
-                # check the rest part
-                for rest in self.partition(s[i:]):
-                    res.append([s[:i]]+rest)
-        return res if res else [[]]
+        dp=[[False for _ in xrange(n)] for _ in xrange(n)]
+        res=[]
+        
+        # check whether the partion is palindrome
+        for cur in xrange(n):
+            for pre in xrange(cur+1):
+                if s[cur]==s[pre] and (cur-pre<=2 or dp[pre+1][cur-1]):
+                    dp[pre][cur]=True
+        self.dfshelper(res,dp,[],0,s)
+        return res
+        
