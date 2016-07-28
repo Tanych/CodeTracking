a3e7f58110815ae4b66c667508d550f0552b0000
Victor <ychtan@email.gwu.edu>
2016-07-28 00:21:00 -0400

    [132. Palindrome Partitioning II][Accepted]committed by Victor

--- 132-Palindrome-Partitioning-II/solution.py
+++ 132-Palindrome-Partitioning-II/solution.py
@@ -1,17 +1,5 @@
 class Solution(object):
-    def dfshelper(self,res,dp,path,pos,strs):
-        # if reach the end of one path
-        n=len(strs)
-        if pos==n:
-            res.append(path-1)
-            return
-        
-        for i in xrange(pos,n):
-            # if currecnt pos is pandirome
-            if dp[pos][i]:
-                # add to path check the rest
-                self.dfshelper(res,dp,path+1,i+1,strs)
-       
+ 
     def minCut(self, s):
         """
         :type s: str
@@ -19,13 +7,26 @@ class Solution(object):
         """
         n=len(s)
         dp=[[False for _ in xrange(n)] for _ in xrange(n)]
-        res=[]
+        cutidx=[0 for _ in xrange(n)]
         
         # check whether the partion is palindrome
         for cur in xrange(n):
+            # in the cur iteration, the max is cut into all single char
+            # so its max its the number of gaps
+            mincut=cur
             for pre in xrange(cur+1):
                 if s[cur]==s[pre] and (cur-pre<=2 or dp[pre+1][cur-1]):
                     dp[pre][cur]=True
-        self.dfshelper(res,dp,0,0,s)
-        return min(res)
+                    # if the s[pre;cur] is palindrome no need to cut
+                    # else we need to calcute the previous index
+                    """
+                    a   c   a   |   a  b a
+                                    j    i
+                           j-1  |  [j, i] is palindrome
+                       cutidx(j-1) +  1
+                    """
+                    mincut=0 if pre==0 else min(mincut,cutidx[pre-1]+1)
+            cutidx[cur]=mincut
+            
+        return cutidx[n-1]
         
