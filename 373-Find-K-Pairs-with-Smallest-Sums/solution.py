ca28110d0fab07ba4740d69b4928dc6d62b7bf22
Victor <ychtan@email.gwu.edu>
2016-07-29 16:14:06 -0400

    [373. Find K Pairs with Smallest Sums][Accepted]committed by Victor

--- 373-Find-K-Pairs-with-Smallest-Sums/solution.py
+++ 373-Find-K-Pairs-with-Smallest-Sums/solution.py
@@ -6,15 +6,26 @@ class Solution(object):
         :type k: int
         :rtype: List[List[int]]
         """
-     
+        n1,n2=len(nums1),len(nums2)
         heapsum=[]
-        for n1 in nums1:
-            for n2 in nums2:
-                heapq.heappush(heapsum,(n1+n2,n1,n2))
+        # push (i,j) into heap
+        def hppush(i,j):
+            if i<n1 and j<n2:
+                heapq.heappush(heapsum,(nums1[i]+nums2[j],i,j))
+        # push the smallest into 
+        hppush(0,0)
+        
         res=[]
-        if heapsum:
+        while heapsum and len(res)<k:
             # k might out of boundary
-            for i in xrange(min(k,len(nums1)*len(nums2))):
-                _,n1,n2=heapq.heappop(heapsum)
-                res.append([n1,n2])
+                _,idx1,idx2=heapq.heappop(heapsum)
+                res.append([nums1[idx1],nums2[idx2]])
+                # push the next smallest into heap
+                hppush(idx1,idx2+1)
+                # next to judge whether the smallest start from nums2 first ele
+                # ex [1,2,3] [4,5,6] first add(5,1,4) and(6,1,5), also (6,2,4)
+                # when the third pop (6,2,4) we consider add (7,3,4) into the heap
+                if idx2==0:
+                    hppush(idx1+1,0)
+                
         return res
