17c32eeb0e177982a14a9299fb1f192027f66e7f
Victor <ychtan@email.gwu.edu>
2016-07-30 11:04:40 -0400

    [239. Sliding Window Maximum][Accepted]committed by Victor

--- 239-Sliding-Window-Maximum/solution.py
+++ 239-Sliding-Window-Maximum/solution.py
@@ -8,13 +8,27 @@ class Solution(object):
         if not nums or k==0:
             return []
         n=len(nums)
-        #queue=collections.deque()
-        #for num in nums:
-        #    queue.append(num)
+        queue=collections.deque()
         res=[]
-        i=0
-        while i<=n-k:
-            res.append(max(nums[i:k+i]))
-            #queue.popleft()
-            i+=1
+        """
+        using the deque to store the idx of the value
+        every time pop out the index out of range
+        """
+        for i in xrange(n):
+            # pop ele out of range
+            while queue and queue[0]<i-k+1:
+                queue.popleft()
+            # discard the num that smaller than the new comer
+            # since it's on the left, it's impossible for them
+            # to be the candicate for the next windows
+            while queue and nums[queue[-1]]<nums[i]:
+                queue.pop()
+                
+            queue.append(i)
+            
+            # get the result, it's the first num in the deque
+            if i>=k-1:
+                res.append(nums[queue[0]])
+                
         return res
+        
