d6966544ec572edf63abd8d754d2aab6f8c6be15
Victor <ychtan@email.gwu.edu>
2016-07-31 13:15:25 -0400

    [191. Number of 1 Bits][Time Limit Exceeded]committed by Victor

---
+++ 191-Number-of-1-Bits/solution.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def hammingWeight(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n==0:
+            return 0
+            
+        i=0
+        count=0
+        while i<32:
+            if n&1:
+                count+=1
+            n=n>>1
+        return count
