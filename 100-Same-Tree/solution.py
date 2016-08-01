c1186eddb21ea823ba760be95af7929734164458
Victor <ychtan@email.gwu.edu>
2016-08-01 16:54:14 -0400

    [100. Same Tree][Accepted]committed by Victor

--- 100-Same-Tree/solution.py
+++ 100-Same-Tree/solution.py
@@ -21,11 +21,16 @@ class Solution(object):
         :type q: TreeNode
         :rtype: bool
         """
+        """
         resp=[]
         resq=[]
         self.tree2str(p,resp)
         self.tree2str(q,resq)
-        #print resp,resq
         return ''.join(resp)==''.join(resq)
+        """
+        # another naive way
+        if p and q:
+            return p.val == q.val and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+        return p==q
         
         
