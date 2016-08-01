ea379bd099ce6265b767867c814b2080b9d62404
Victor <ychtan@email.gwu.edu>
2016-08-01 15:39:36 -0400

    [104. Maximum Depth of Binary Tree][Accepted]committed by Victor

--- 104-Maximum-Depth-of-Binary-Tree/solution.py
+++ 104-Maximum-Depth-of-Binary-Tree/solution.py
@@ -21,6 +21,9 @@ class Solution(object):
         # the recursive method is very straightforward
         # in real or interview, need to implement using stack and some advanced traval tech
         # not level by level
+        """
+        ### simple bfs
+
         stck=[root]
         depth=0
         # bfs search
@@ -34,4 +37,35 @@ class Solution(object):
                     tmp.append(cur.right)
             stck=tmp
         return depth
+        """
+        
+        ### implement not level by level
+        stck=[root]
+        depth=0
+        # record the previous route
+        prenode=None
+        
+        while stck:
+            cur=stck[-1]
+            #  if prenode is above cur node
+            if not prenode or prenode.left==cur or prenode.right==cur:
+                # left has priority to add, if--elif not if---if 
+                if cur.left:
+                    stck.append(cur.left)
+                elif cur.right:
+                    stck.append(cur.right)
+            
+            # else if the travel back from bottom, the left subtree is completed
+            elif cur.left==prenode:
+                if cur.right:
+                    stck.append(cur.right)
+            # reach the leaf node, prenode and cur reach the same node
+            else:
+                stck.pop()
                 
+            # move pre to next route     
+            prenode=cur
+            depth=max(depth,len(stck))
+            
+        return depth
+        
