19f273bf299a265b958c62646e403a81d923706a
Victor <ychtan@email.gwu.edu>
2016-08-02 15:37:27 -0400

    [332. Reconstruct Itinerary][Accepted]committed by Victor

--- 332-Reconstruct-Itinerary/solution.py
+++ 332-Reconstruct-Itinerary/solution.py
@@ -1,4 +1,17 @@
 class Solution(object):
+    def __init__(self):
+        self.path=[]
+        
+    def dfs(self,t_graph,node):
+        arrivals=[]
+        
+        if node in t_graph:
+            arrivals=t_graph[node]
+        # pay attention to that at least one valid itinerary
+        while arrivals:
+            self.dfs(t_graph,arrivals.pop())
+        self.path.append(node)
+        
     def findItinerary(self, tickets):
         """
         :type tickets: List[List[str]]
@@ -12,13 +25,19 @@ class Solution(object):
                 t_graph[t[0]]=sorted(t_graph[t[0]],reverse=True)
             else:
                 t_graph[t[0]]=[t[1]]
-                
-        route,stck=[],['JFK']
+             
+        # recusive
+        self.dfs(t_graph,'JFK')
+        return self.path[::-1]
+        
+        # using stack
+        """
+        path,stck=[],['JFK']
         while stck:
             # iterative get the node from the graph
             while stck[-1] in t_graph and t_graph[stck[-1]]:
                 stck.append(t_graph[stck[-1]].pop())
-            # stck is the route, now route is the reverse when do pop()
-            route.append(stck.pop())
-        return route[::-1]
-            
+            # stck is the path, now path is the reverse when do pop()
+            path.append(stck.pop())
+        return path[::-1]
+        """ 
