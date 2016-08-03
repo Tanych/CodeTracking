9a7b52ba95fb4019979ba4c086ae06057b81a4dc
Victor <ychtan@email.gwu.edu>
2016-08-03 16:02:09 -0400

    [241. Different Ways to Add Parentheses][Accepted]committed by Victor

--- 241-Different-Ways-to-Add-Parentheses/solution.py
+++ 241-Different-Ways-to-Add-Parentheses/solution.py
@@ -1,5 +1,36 @@
 class Solution(object):
-    def diffWaysToCompute(self, input):
+    def diffWaysToCompute(self,input):
+        """
+        define dp[i][j] is the result from the expression from i to j
+        dp[j][j]=input[j] for j in odd position
+        """
+        import re
+        input = re.split('(\+|\-|\*|\/)', input)
+        n=len(input)
+        dp=[[[] for _ in xrange(n)] for _ in xrange(n)]
+        
+        # i means the total length of the expression
+        # EX: 3-4*5+6*7, i=3, there could be 3-4,4*5,5+6,6*7.
+        for i in xrange(1,n+1,2):
+            # start from num j, it could be the total possible expression based on the length of i
+            # so j need stop at the length of n-i+1, to make sure the last has i length
+            for j in xrange(0,n+1-i,2):
+                # the total length of expression could be 1, and it only should contains numbers
+                if i==1:
+                    dp[j][j+i-1].append(eval(input[j]))
+                else:
+                    # k could be the operator in all the possible expression
+                    # if from i,j the expression is  3-4*5+6*7, k could be - ,* ,+,*
+                    # for the i length expression, the last op is j+i-1
+                    for k in xrange(j+1,j+i-1,2):
+                        left=dp[j][k-1]; right=dp[k+1][j+i-1];
+                        for num1 in left:
+                            for num2 in right:
+                                dp[j][j+i-1].append(eval(str(num1)+input[k]+str(num2)))
+        return dp[0][n-1]
+                        
+        
+    def diffWaysToCompute_rec(self, input):
         """
         :type input: str
         :rtype: List[int]
@@ -23,4 +54,5 @@ class Solution(object):
                 mapping[input]=res
                 
         return res if len(res) else [int(input)]
+
         
