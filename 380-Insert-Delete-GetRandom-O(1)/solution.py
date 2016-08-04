16ce572599aecac2954fb7a92d4e8ab25a76f0cf
Victor <ychtan@email.gwu.edu>
2016-08-04 16:44:11 -0400

    [380. Insert Delete GetRandom O(1)][Accepted]committed by Victor

---
+++ 380-Insert-Delete-GetRandom-O(1)/solution.py
@@ -0,0 +1,48 @@
+class RandomizedSet(object):
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.valset=[]
+
+    def insert(self, val):
+        """
+        Inserts a value to the set. Returns true if the set did not already contain the specified element.
+        :type val: int
+        :rtype: bool
+        """
+        if val in self.valset:
+            return False
+        else:
+            self.valset.append(val)
+            return True
+        
+
+    def remove(self, val):
+        """
+        Removes a value from the set. Returns true if the set contained the specified element.
+        :type val: int
+        :rtype: bool
+        """
+        if val in self.valset:
+            self.valset.remove(val)
+            return True
+        else:
+            return False
+        
+
+    def getRandom(self):
+        """
+        Get a random element from the set.
+        :rtype: int
+        """
+        import random
+        return self.valset[random.randint(0, len(self.valset)-1)]
+
+
+# Your RandomizedSet object will be instantiated and called as such:
+# obj = RandomizedSet()
+# param_1 = obj.insert(val)
+# param_2 = obj.remove(val)
+# param_3 = obj.getRandom()
