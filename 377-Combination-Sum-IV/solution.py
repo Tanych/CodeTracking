0c41551c30cb594de3f3901f4ff67f20a57077b7
Victor <ychtan@email.gwu.edu>
2016-08-04 16:00:01 -0400

    [377. Combination Sum IV][Accepted]committed by Victor

--- 377-Combination-Sum-IV/solution.py
+++ 377-Combination-Sum-IV/solution.py
@@ -2,30 +2,34 @@ class Solution(object):
     def __init__(self):
         self.cnt=0
         
-    def helper(self,nums,res,target):
-        if res==target:
-           self.cnt+=1
-           return
-        
-        if res>target:
-            return
-        
+    def helper(self,nums,target,mapping):
+        if mapping[target]!=-1:
+            return mapping[target]
+        res=0
         for num in nums:
-            self.helper(nums,res+num,target)
+            if target>=num:
+                res+=self.helper(nums,target-num,mapping)
+        mapping[target]=res
+        return res
             
-    def combinationSum4(self, nums, target):
+    def combinationSum4_up(self, nums, target):
         """
         :type nums: List[int]
         :type target: int
         :rtype: int
         """
+        mapping=[-1]*(target+1)
+        mapping[0]=1
+        self.helper(nums,target,mapping)
+        return mapping[target]
+    
+    def combinationSum4(self, nums, target):
+        dp=[0]*(target+1)
+        dp[0]=1
         
-        if not nums:
-            return 0
+        for i in xrange(1,len(dp)):
+            for j in xrange(len(nums)):
+                if i>=nums[j]:
+                    dp[i]+=dp[i-nums[j]]
+        return dp[target]
             
-        if len(nums)==1 and nums[0]<target:
-            return 0
-            
-        self.helper(nums,0,target)
-        return self.cnt
-        
