2bd91ec9debb048dba5923ffda176aae1fc18af0
Victor <ychtan@email.gwu.edu>
2016-08-05 14:45:07 -0400

    [331. Verify Preorder Serialization of a Binary Tree][Accepted]committed by Victor

--- 331-Verify-Preorder-Serialization-of-a-Binary-Tree/solution.py
+++ 331-Verify-Preorder-Serialization-of-a-Binary-Tree/solution.py
@@ -18,10 +18,33 @@ class Solution(object):
         :type preorder: str
         :rtype: bool
         """
+        return self.isValidSerialization_stck(preorder)
+        
         n=len(preorder)
         
         # the nodes lists
         nodes=preorder.split(',')
         return self.helper(nodes,0)==len(nodes)-1
+    
+    def isValidSerialization_stck(self,preorder):
+        n=len(preorder)
+        
+        # the nodes lists
+        nodes=preorder.split(',')
+        stck=[]
+        for i in xrange(len(nodes)):
+            cur=nodes[i]
+            while cur=='#' and stck and stck[-1]=='#':
+                # pop the '#'
+                stck.pop()
+                if not stck:
+                    return False
+                # pop the number
+                stck.pop()
+            # add the '#' after pop in order to do next
+            # or add number
+            stck.append(cur)
+            
+        return len(stck)==1 and stck[0]=='#'
             
                 
