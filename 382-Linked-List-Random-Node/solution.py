8c65443a2f1df0c2e18ad04250de474f603c6dc9
Victor <ychtan@email.gwu.edu>
2016-08-11 10:36:01 -0400

    [382. Linked List Random Node][Accepted]committed by Victor

--- 382-Linked-List-Random-Node/solution.py
+++ 382-Linked-List-Random-Node/solution.py
@@ -13,21 +13,40 @@ class Solution(object):
         """
         self.head=head
         
-
+    def getKitem(self,k):
+        cur=self.head
+        i,res=0,[0]*k
+        while cur and i<k:
+            res[i]=cur.val
+            cur=cur.next
+            i+=1
+        
+        while cur:
+            # select j from 0-i
+            j=random.randint(0, i)
+            # if the pick index less than k, replace it
+            if j<k:
+                res[j]=cur.val
+            cur=cur.next
+            i+=1
+        return res
+    
+        
     def getRandom(self):
         """
         Returns a random node's value.
         :rtype: int
         """
-        n=0
-        node=self.head
+        res=self.head.val
+        node=self.head.next
+        i=2
         while node:
+            j=random.randint(0, i-1)
+            if j<1:
+                res=node.val
+            i+=1
             node=node.next
-            n+=1
-        node=self.head
-        for i in xrange(random.randrange(n)):
-            node=node.next
-        return node.val
+        return res
         
 
 
