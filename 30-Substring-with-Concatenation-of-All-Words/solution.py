8b744cb3c9cbc84cfe8100e421ca7923f00c54f6
Victor <ychtan@email.gwu.edu>
2016-08-12 16:18:54 -0400

    [30. Substring with Concatenation of All Words][Accepted]committed by Victor

--- 30-Substring-with-Concatenation-of-All-Words/solution.py
+++ 30-Substring-with-Concatenation-of-All-Words/solution.py
@@ -1,10 +1,51 @@
 class Solution(object):
+    def _findsub(self,left,right,slen,wlen,t_wlen,cntword,res,s):
+        curr={}
+        while right+wlen<=slen:
+            word=s[right:right+wlen]
+            right+=wlen
+            
+            if word not in cntword:
+                # start new pos
+                left=right
+                curr.clear()
+            else:
+                # cnt currurent word
+                curr[word]=curr.get(word,0)+1
+                # if current has more mutilp words, get rid of the left part
+                while curr[word]>cntword[word]:
+                    # the left word should get out
+                    curr[s[left:left+wlen]]-=1
+                    # move left
+                    left+=wlen
+                # if the length equal the total tagert
+                if right-left==t_wlen:
+                    res.append(left)
+            
+    def findsubstring_dict(self, s,words):
+        str_len=len(s)
+        word_len=len(words[0])
+        total_wlen=word_len*len(words)
+        
+        # building dict for the words
+        cntword={}
+        for word in words:
+            cntword[word]=cntword.get(word,0)+1
+        
+        res=[]
+        for i in xrange(min(word_len,str_len-total_wlen+1)):
+            self._findsub(i,i,str_len,word_len,total_wlen,cntword,res,s)
+        return res
+        
+        
     def findSubstring(self, s, words):
         """
         :type s: str
         :type words: List[str]
         :rtype: List[int]
         """
+        return self.findsubstring_dict(s,words)
+        
         # the length relate to the solution
         str_len=len(s)
         word_len=len(words[0])
