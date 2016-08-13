87c7ba8f168474b29bc80d43157ea0ad81548c77
Victor <ychtan@email.gwu.edu>
2016-08-13 19:14:46 -0400

    [37. Sudoku Solver][Accepted]committed by Victor

--- 37-Sudoku-Solver/solution.py
+++ 37-Sudoku-Solver/solution.py
@@ -1,39 +1,58 @@
 class Solution(object):
-    def isValidSudoku(self, board,row,col, ch):
-        
+    def __init__(self):
+        self.trow=[0]*9
+        self.tcol=[0]*9
+        self.block=[0]*9
+    
+    def initstate(self,board):
         for i in xrange(9):
-            if board[i][col]==str(ch):
-                return False
-        for j in xrange(9):
-            if board[row][j]==str(ch):
-                return False
-        for i in xrange((row/3)*3,(row/3)*3+3):
-            for j in xrange((col/3)*3,(col/3)*3+3):
-                if board[i][j]==str(ch):
-                    return False
+            for j in xrange(9):
+                if board[i][j]!='.':
+                   self.setstate(board,i,j)
+                    
+    def setstate(self,board,i,j):
+        idx=1<<int(board[i][j])
+        self.trow[i]|=idx
+        self.tcol[j]|=idx
+        self.block[(i/3)*3+(j/3)]|=idx
+        
+    def unsetstate(self,board,i,j):
+        idx=1<<int(board[i][j])
+        self.trow[i]&=~idx
+        self.tcol[j]&=~idx
+        self.block[(i/3)*3+(j/3)]&=~idx
+    
+    def isValidSudoku(self,board,i,j):
+        idx=1<<int(board[i][j])
+        if self.trow[i]&idx or self.tcol[j]&idx or self.block[(i/3)*3+(j/3)]&idx:
+            return False
         return True
-            
         
     def solveSudoku(self, board):
         """
         :type board: List[List[str]]
         :rtype: void Do not return anything, modify board in-place instead.
         """
-        self.solve(board)
+        self.initstate(board)
+        self.dfssolve(board,0,0)
         
-    def solve(self,board):
-        # first try brute force
-        for i in xrange(9):
-            for j in xrange(9):
-                if board[i][j]=='.':
-                    for k in xrange(1,10):
-                        if self.isValidSudoku(board,i,j,k):
-                            board[i]=board[i][:j]+[str(k)]+board[i][j+1:]
-                            if self.solve(board):
-                                return True
-                            else:
-                                board[i]=board[i][:j]+['.']+board[i][j+1:]
-                    return False
-        return True
-        
+    def dfssolve(self,board,i,j):
+        if i>8:
+            return True
+        if j>8:
+            return self.dfssolve(board,i+1,0)
+                
+        if board[i][j]=='.':
+            for k in xrange(1,10):
+                board[i][j]=str(k)
+                if self.isValidSudoku(board,i,j):
+                    self.setstate(board,i,j)
+                    if self.dfssolve(board,i,j+1):
+                        return True
+                    self.unsetstate(board,i,j)
+                
+            board[i][j]='.'
+            return False
+        else:
+            return self.dfssolve(board,i,j+1)
         
