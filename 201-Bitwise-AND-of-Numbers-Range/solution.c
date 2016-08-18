f63cd0e6ffd256d1a34a2a00ef71284aa48a213b
Victor <Tanych@users.noreply.github.com>
2016-08-18 14:37:13 -0400

    [201. Bitwise AND of Numbers Range][Accepted]committed by Victor

---
+++ 201-Bitwise-AND-of-Numbers-Range/solution.c
@@ -0,0 +1,13 @@
+int rangeBitwiseAnd(int m, int n) {
+ 
+    int offset=0;
+    while(m&&n)
+    {
+        if(m==n)
+            return m<<offset;
+        m>>=1;
+        n>>=1;
+        offset++;
+    }
+    return 0;
+}
