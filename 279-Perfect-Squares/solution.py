21194d28580ab952baa2d37c44327f7c7d6e77ee
Victor <Tanych@users.noreply.github.com>
2016-08-19 16:54:30 -0400

    [279. Perfect Squares][Time Limit Exceeded]committed by Victor

--- 279-Perfect-Squares/solution.py
+++ 279-Perfect-Squares/solution.py
@@ -1,6 +1,21 @@
 class Solution(object):
     def __init__(self):
         self._dp=[0]
+    
+    def dpsolution(self,n):
+        if n<=0:
+            return 0
+        
+        dp=[1<<31]*(n+1)
+        dp[0]=0
+        for k in xrange(1,n+1):
+            #For each i, it must be the sum of some number (i - j*j) and 
+            #a perfect square number (j*j)
+            j=1
+            while j*j<=k:
+                dp[k]=min(dp[k],dp[k-j*j]+1)
+                j+=1
+        return dp[-1]
         
     def numSquares(self, n):
         """
@@ -11,6 +26,8 @@ class Solution(object):
         n=a+b*b
         dp[a+b*b]=min(dp[a]+1,dp[a+b*b])
         """
+        return self.dpsolution(n)
+        
         dp=self._dp
         while len(dp) <= n:
             dp += min(dp[-i*i] for i in range(1, int(len(dp)**0.5+1))) + 1,
