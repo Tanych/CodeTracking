8cf2c583920edf2cb23b389ca942a51b4174e3c1
Victor <Tanych@users.noreply.github.com>
2016-08-19 20:13:26 -0400

    [279. Perfect Squares][Accepted]committed by Victor

--- 279-Perfect-Squares/solution.py
+++ 279-Perfect-Squares/solution.py
@@ -1,27 +1,46 @@
 class Solution(object):
+    """
     def __init__(self):
         self._dp=[0]
-    
-    def dpsolution(self,n):
+    """
+    dp=[0]
+    def numSquares(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        for i in range(len(self.dp), n+1):
+            result = sys.maxsize
+            k = int(math.sqrt(i))
+            if k * k == i:
+                self.dp.append(1)
+                continue
+            for j in range(1, int(math.sqrt(i)) + 1):
+                result = min(result, self.dp[i - j * j] + 1)
+                if result is 2:
+                    break
+            self.dp.append(result)
+        return self.dp[n]
+        
+    def numSquares1(self,n):
         # DP TLE....
         if n<=0:
             return 0
-        dp=self._dp
         
         for i in xrange(1,n+1):
-            dp+=[1<<31]
+            self.dp+=[1<<31]
             
-        dp[0]=0
+        self.dp[0]=0
         for k in xrange(1,n+1):
             #For each i, it must be the sum of some number (i - j*j) and 
             #a perfect square number (j*j)
             j=1
             while j*j<=k:
-                dp[k]=min(dp[k],dp[k-j*j]+1)
+                self.dp[k]=min(self.dp[k],self.dp[k-j*j]+1)
                 j+=1
-        return dp[-1]
+        return self.dp[-1]
         
-    def numSquares(self, n):
+    def numSquares2(self, n):
         """
         :type n: int
         :rtype: int
@@ -30,7 +49,7 @@ class Solution(object):
         n=a+b*b
         dp[a+b*b]=min(dp[a]+1,dp[a+b*b])
         """
-        #return self.dpsolution(n)
+        return self.dpsolution(n)
         
         dp=self._dp
         while len(dp) <= n:
