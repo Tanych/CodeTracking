b96d23a68364e9ef88886e58e94fe43229297b08
Victor <Tanych@users.noreply.github.com>
2016-08-22 19:56:32 -0400

    [386. Lexicographical Numbers][Accepted]committed by Victor

--- 386-Lexicographical-Numbers/solution.py
+++ 386-Lexicographical-Numbers/solution.py
@@ -7,12 +7,30 @@ class Solution(object):
         if num<=n:
             for i in xrange(10):
                 self._dfs(num+i,res,n)
+    def sovleOn(self,n):
+        res=[]
+        cur=1
+        for i in xrange(1,n+1):
+            res.append(cur)
+            if cur*10<=n:
+                cur=cur*10
+            # if the num not end with 9,plus 1
+            # since if 19 the next should 2 not 20
+            elif cur%10!=9 and cur+1<=n:
+                cur+=1
+            else:
+                # get the 199--2 499--5
+                while (cur/10)%10==9:
+                    cur/=10
+                cur=cur/10+1
+        return res
                 
     def lexicalOrder(self, n):
         """
         :type n: int
         :rtype: List[int]
         """
+        return self.sovleOn(n)
         res=[]
         for i in xrange(1,10):
             self._dfs(i,res,n)
