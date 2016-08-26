86afc911a71f65d1b12b8221af0b9013cb543b04
Victor <Tanych@users.noreply.github.com>
2016-08-26 15:10:02 -0400

    [98. Validate Binary Search Tree][Accepted]committed by Victor

--- 98-Validate-Binary-Search-Tree/solution.py
+++ 98-Validate-Binary-Search-Tree/solution.py
@@ -5,7 +5,45 @@
 #         self.left = None
 #         self.right = None
 
+class TreeCnt(object):
+    def __init__(self,cnt,mn,mx):
+        self.cnt=cnt
+        self.mn=mn
+        self.mx=mx
+        
 class Solution(object):
+    def __init__(self):
+        self.bstmax=1
+        
+    def isValidBST_333(self, root):
+        # solution for #333
+        if not root:
+            return 0
+        self.isvalid(root)
+        return self.bstmax
+    
+    def isvalid(self,root):
+        if not root:
+            return TreeCnt(-1,-1,-1)
+        if not root.left and not root.right:
+            return TreeCnt(1,root.val,root.val)
+        
+        mid=root.val
+        left=self.isvalid(root.left)
+        right=self.isvalid(root.right)
+        
+        if left.cnt>0 and right.cnt>0 and left.mx<mid and right.mn>mid:
+            self.bstmax=max(self.bstmax,left.cnt+right.cnt+1)
+            return TreeCnt(left.cnt+right.cnt+1,left.mn,right.mx)
+        # only with right subtree
+        if left.cnt==-1 and right.cnt>0 and right.mn>mid:
+            self.bstmax=max(self.bstmax,right.cnt+1)
+            return TreeCnt(right.cnt+1,mid,right.mx)
+        #only with left subtrees
+        if right.cnt==-1 and left.cnt>0 and left.mx<mid:
+            self.bstmax=max(self.bstmax,left.cnt+1)
+            return TreeCnt(left.cnt+1,left.mn,mid)
+        
     def isValidBST(self, root):
         """
         :type root: TreeNode
