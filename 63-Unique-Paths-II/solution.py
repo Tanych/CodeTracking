c38b263e50f95e64f0ae764c63fec702d9957c7e
Victor <Tanych@users.noreply.github.com>
2016-08-27 22:02:57 -0400

    [63. Unique Paths II][Accepted]committed by Victor

--- 63-Unique-Paths-II/solution.py
+++ 63-Unique-Paths-II/solution.py
@@ -4,36 +4,15 @@ class Solution(object):
         :type obstacleGrid: List[List[int]]
         :rtype: int
         """
-        # to make the O(n) space 
         row=len(obstacleGrid)
         col=len(obstacleGrid[0])
-      
-        if obstacleGrid[row-1][col-1] == 1 or obstacleGrid[0][0] == 1:
-            return 0
-                
-        dp=[0 for _ in xrange(col)]
-        dp[0]=1
         
-        # deal with the first row--if only one row
-        for j in xrange(1,col):
-            if obstacleGrid[0][j]==1:
-                dp[j]=0
-            else:
-                dp[j]=dp[j-1]
-            
-        # check the dp[i][j]..the 1---row
-        for i in xrange(1,row):
-            for j in xrange(0,col):
-                # deal with the first column
-                if j==0:
-                    if obstacleGrid[i][j]==1:
-                        dp[j]=0
-                else:
-                    # if face obstacle, set dp[i][j] to 0
-                    if obstacleGrid[i][j]==1:
-                        dp[j]=0
-                    else:
-                        dp[j]=dp[j]+dp[j-1]
-                    
-        return dp[col-1]
+        dp=[[0 for _ in xrange(col+1)] for _ in xrange(row+1)]
+        dp[0][1]=1
+        # check the dp[i][j]
+        for i in xrange(1,row+1):
+            for j in xrange(1,col+1):
+                if obstacleGrid[i-1][j-1]==0:
+                    dp[i][j]=dp[i-1][j]+dp[i][j-1]
+        return dp[row][col]
         
