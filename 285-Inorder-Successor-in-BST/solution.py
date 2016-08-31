0cb9aae5fed88484b3509f4b670974681748e3cb
Victor <Tanych@users.noreply.github.com>
2016-08-31 16:10:02 -0400

    [285. Inorder Successor in BST][Accepted]committed by Victor

--- 285-Inorder-Successor-in-BST/solution.py
+++ 285-Inorder-Successor-in-BST/solution.py
@@ -6,12 +6,32 @@
 #         self.right = None
 
 class Solution(object):
+    def recursive(self,root,p):
+        if not root:
+            return None
+        if p.val<root.val:
+            left=self.recursive(root.left,p)
+            return root if not left else left
+        else:
+            return self.recursive(root.right,p)
+            
+    def inorderpreoder(self,root,p):
+        pre=None
+        while root:
+            if p.val>root.val:
+                pre=root
+                root=root.right
+            else:
+                root=root.left
+        return pre
+        
     def inorderSuccessor(self, root, p):
         """
         :type root: TreeNode
         :type p: TreeNode
         :rtype: TreeNode
         """
+        return self.recursive(root,p)
         suc=None
         while root:
             if p.val<root.val:
