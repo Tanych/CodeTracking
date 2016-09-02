b9d8d22693a69bc7f3481fb5f6fca331a21c819a
Victor <Tanych@users.noreply.github.com>
2016-09-02 15:31:54 -0400

    [158. Read N Characters Given Read4 II - Call multiple times][Time Limit Exceeded]committed by Victor

---
+++ 158-Read-N-Characters-Given-Read4-II---Call-multiple-times/solution.py
@@ -0,0 +1,42 @@
+# The read4 API is already defined for you.
+# @param buf, a list of characters
+# @return an integer
+# def read4(buf):
+
+class Solution(object):
+    def __init__(self):
+        self.remain=[]
+        
+    def read(self, buf, n):
+        """
+        :type buf: Destination buffer (List[str])
+        :type n: Maximum number of characters to read (int)
+        :rtype: The number of characters read (int)
+        """
+        i=0
+        while i<n and self.remain:
+            buf[i]=self.remain.pop(0)
+            i+=1
+        
+        while i<n:
+            tmp=[0]*4
+            nlen=read4(tmp)
+            if n-i<nlen:
+                # copy buf needed
+                j=0
+                while j<n-i:
+                    buf[i+j]=tmp[j]
+                    j+=1
+                # copy the left
+                while j<nlen:
+                    self.remain.append(tmp[j])
+            else:
+                # copy all to the buf
+                for j in xrange(nlen):
+                    buf[i+j]=tmp[j]
+            # return the min extra length
+            if nlen<4:
+                return min(i+nlen,n)
+            i+=4
+        return n
+        
