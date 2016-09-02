a84fa28af5c901434fb6a6f5abb2dd59b79769a3
Victor <Tanych@users.noreply.github.com>
2016-09-02 17:06:10 -0400

    [163. Missing Ranges][Accepted]committed by Victor

--- 163-Missing-Ranges/solution.py
+++ 163-Missing-Ranges/solution.py
@@ -6,30 +6,11 @@ class Solution(object):
         :type upper: int
         :rtype: List[str]
         """
-        if lower>upper:
-            return []
-            
-        if not nums:
-            if upper==lower:
-                return [str(lower)]
-            else:
-                return [str(lower)+'->'+str(upper)]
-        
-        res=[]
-        if nums[0]-lower==1:
-            res.append(str(lower))
-        elif nums[0]-lower>=2:
-            res.append(str(lower)+'->'+str(nums[0]-1))
-            
-        for i in xrange(1,len(nums)):
-            if nums[i]-nums[i-1]==2:
-                res.append(str(nums[i-1]+1))
-            elif nums[i]-nums[i-1]>2:
-                res.append(str(nums[i-1]+1)+'->'+str(nums[i]-1))
-        
-        if upper-nums[-1]==1:
-            res.append(str(upper))
-        elif upper-nums[-1]>=2:
-            res.append(str(nums[-1]+1)+'->'+str(upper))
+        rangexy=lambda x,y:str(x) if x==y else str(x)+'->'+str(y)
+        pre,cur,n,res=lower-1,0,len(nums),[]
+        for i in xrange(n+1):
+            cur=upper+1 if i==n else nums[i]
+            if cur-pre>1:
+                res.append(rangexy(pre+1,cur-1))
+            pre=cur
         return res
-        
