a5afa894565f7d3aa420b40ed208c784206abd41
Victor <Tanych@users.noreply.github.com>
2016-09-04 23:38:24 -0400

    [395. Longest Substring with At Least K Repeating Characters][Accepted]committed by Victor

---
+++ 395-Longest-Substring-with-At-Least-K-Repeating-Characters/solution.py
@@ -0,0 +1,36 @@
+class Solution(object):
+    def longestSubstring(self, s, k):
+        """
+        :type s: str
+        :type k: int
+        :rtype: int
+        """
+        charcnt=[0]*26
+        charset=set()
+        
+        for ch in s:
+            charcnt[ord(ch)-97]+=1
+            if ch not in charset:
+                charset.add(ch)
+        for i in xrange(26):
+            if charcnt[i]>=k:
+                charset.discard(chr(i+97))
+        if not charset:
+            return len(s)
+        splits,sp=[],0
+        while sp<len(s):
+            if s[sp] not in charset:
+                i=sp
+                while sp<len(s):
+                    if s[sp] not in charset:
+                        sp+=1
+                    else:
+                        break
+                splits.append((i,sp))
+            else:
+                sp+=1
+        maxlen=0
+        for ele in splits:
+            maxlen=max(maxlen,self.longestSubstring(s[ele[0]:ele[1]],k))
+        return maxlen
+                
