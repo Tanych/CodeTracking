8d44547c170f3d253b826a6f75e5bcc10b32a6c7
Victor <Tanych@users.noreply.github.com>
2016-09-04 23:43:11 -0400

    [395. Longest Substring with At Least K Repeating Characters][Accepted]committed by Victor

--- 395-Longest-Substring-with-At-Least-K-Repeating-Characters/solution.py
+++ 395-Longest-Substring-with-At-Least-K-Repeating-Characters/solution.py
@@ -1,10 +1,19 @@
 class Solution(object):
+    def pythonic(self,s,k):
+        if len(s)<k:
+            return 0
+        minchar=min(set(s),key=s.count)
+        if s.count(minchar)>=k:
+            return len(s)
+        return max(self.pythonic(t,k) for t in s.split(minchar))
+        
     def longestSubstring(self, s, k):
         """
         :type s: str
         :type k: int
         :rtype: int
         """
+        return self.pythonic(s,k)
         charcnt=[0]*26
         charset=set()
         
