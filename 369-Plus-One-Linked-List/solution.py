9c5c60b52e6b121a92992d88f109d6760a00151e
Victor <Tanych@users.noreply.github.com>
2016-09-07 11:09:37 -0400

    [369. Plus One Linked List][Accepted]committed by Victor

--- 369-Plus-One-Linked-List/solution.py
+++ 369-Plus-One-Linked-List/solution.py
@@ -14,11 +14,52 @@ class Solution(object):
             cur=nextp
         return pre
         
+    def nomodify(self,head):
+        node=head
+        hascarry=False
+        # record the consecuctive 9 start point
+        spos=0
+        idx=0
+        # record the previous number
+        pre=0
+        while node:
+            if node.val!=9:
+                pre=0
+            # find the first 9
+            if pre!=9 and node.val==9:
+                pre=9
+                spos=idx
+            if node.next is None and node.val==9:
+                hascarry=True
+            idx+=1
+            node=node.next
+        # if no carry update to max len
+        if not hascarry:
+            spos=idx
+            
+        # start update from the consecutive 9
+        node=head
+        idx=0
+        while node:
+            # 2-9-9 update from spos-1
+            if spos-1<=idx:
+                node.val=(node.val+1)%10
+            idx+=1
+            node=node.next
+        # add the 1 for 9-9-9
+        if hascarry and spos==0:
+            newhead=ListNode(1)
+            newhead.next=head
+            head=newhead
+        return head
+        
+        
     def plusOne(self, head):
         """
         :type head: ListNode
         :rtype: ListNode
         """
+        return self.nomodify(head)
         carry=1
         newhead=self.reverse(head)
         cur=newhead
