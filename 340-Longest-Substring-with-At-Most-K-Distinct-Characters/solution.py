e332b45586ec93149f13ad8f039b8e5ad64b5ca3
Victor <Tanych@users.noreply.github.com>
2016-09-10 14:08:36 -0400

    [340. Longest Substring with At Most K Distinct Characters][Accepted]committed by Victor

--- 340-Longest-Substring-with-At-Most-K-Distinct-Characters/solution.py
+++ 340-Longest-Substring-with-At-Most-K-Distinct-Characters/solution.py
@@ -1,10 +1,43 @@
 class Solution(object):
+    def followup(self,s,k):
+        """
+        the question follow up is data char, only can read one char
+        """
+        if not s or k==0:
+            return 0
+        # using the maxheap to save the lastoccurence of the char
+        maxheap=[]
+        # window size record the k distince char
+        inwindow={}
+        j,maxlen=0,1
+        # also can read char from the api
+        for i in xrange(len(s)):
+            ch=s[i]
+            if len(inwindow)==k and (ch not in inwindow):
+                idx,first=heapq.heappop(maxheap)
+                del inwindow[first]
+                j=idx+1
+            if ch in inwindow:
+                #del maxheap[-inwindow[ch]]
+                for idx in xrange(len(maxheap)):
+                    if maxheap[idx][1]==ch:
+                        maxheap[idx],maxheap[-1]=maxheap[-1],maxheap[idx]
+                        maxheap.pop()
+                        heapq.heapify(maxheap)
+                        break
+            inwindow[ch]=i
+            heapq.heappush(maxheap,(i,ch))
+            maxlen=max(maxlen,(i-j+1))
+        return maxlen
+            
+        
     def lengthOfLongestSubstringKDistinct(self, s, k):
         """
         :type s: str
         :type k: int
         :rtype: int
         """
+        return self.followup(s,k)
         strcnt=[0]*128
         res=cnt=left=right=0
         while right<len(s):
