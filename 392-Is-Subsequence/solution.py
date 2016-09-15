ef359f5e9030f78ec14dc3bf1044064dca4b0b72
Victor <Tanych@users.noreply.github.com>
2016-09-15 12:33:02 -0400

    [392. Is Subsequence][Accepted]committed by Victor

--- 392-Is-Subsequence/solution.py
+++ 392-Is-Subsequence/solution.py
@@ -30,24 +30,24 @@ class Solution(object):
         return (i,temp)
         
     def manys(self,s,t):
-        hmaping={}
+        hmaping=[[] for _ in range(26)]
         # pre record the position
         for i in xrange(len(t)):
-            hmaping[t[i]]=hmaping.get(t[i],[])+[i]
+            hmaping[ord(t[i])- 97].append(i)
         
         # search index
         curidx=-1
         for ch in s:
-            if ch not in hmaping or hmaping[ch][-1]<=curidx:
+            if not hmaping[ord(ch)-97] or hmaping[ord(ch)-97][-1]<=curidx:
                 return False
-            left,right=0,len(hmaping[ch])-1
+            left,right=0,len(hmaping[ord(ch)-97])-1
             while left<right:
                 mid=(left+right)/2
-                if hmaping[ch][mid]>curidx:
+                if hmaping[ord(ch)-97][mid]>curidx:
                     right=mid
                 else:
                     left=mid+1
-            curidx=hmaping[ch][left]
+            curidx=hmaping[ord(ch)-97][left]
         return True
                 
     def isSubsequence(self, s, t):
@@ -56,7 +56,7 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
-        return self.isSubsequence1(s,t)
+        return self.manys(s,t)
         sidx=tidx=0
         while sidx<len(s) and tidx<len(t):
             if s[sidx]==t[tidx]:
