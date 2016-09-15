3a70cbc19ef52045edff80f586e6f2f9a3fbac03
Victor <Tanych@users.noreply.github.com>
2016-09-15 12:18:19 -0400

    [392. Is Subsequence][Time Limit Exceeded]committed by Victor

--- 392-Is-Subsequence/solution.py
+++ 392-Is-Subsequence/solution.py
@@ -1,10 +1,39 @@
 class Solution(object):
+    def manys(self,s,t):
+        hmaping={}
+        # pre record the position
+        for i in xrange(len(t)):
+            hmaping[t[i]]=hmaping.get(t[i],[])+[i]
+        
+        # search index
+        curidx=-1
+        for ch in s:
+            if ch not in hmaping or hmaping[ch][-1]<=curidx:
+                return False
+            left,right=0,len(hmaping[ch])-1
+            while left<right:
+                mid=(left+right)/2
+                if hmaping[ch][mid]>curidx:
+                    right=mid
+                else:
+                    left=mid+1
+            curidx=hmaping[ch][left]
+        return True
+                
     def isSubsequence(self, s, t):
         """
         :type s: str
         :type t: str
         :rtype: bool
         """
+        return self.manys(s,t)
+        sidx=tidx=0
+        while sidx<len(s) and tidx<len(t):
+            if s[sidx]==t[tidx]:
+                sidx+=1
+            tidx+=1
+        return sidx==len(s)
+        
         i=0
         for ch in s:
             newi=t.find(ch,i)
