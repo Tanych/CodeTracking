d59ffda3b3f96c91684b5b02bec536ed18ec5798
Victor <Tanych@users.noreply.github.com>
2016-09-16 10:50:58 -0400

    [322. Coin Change][Accepted]committed by Victor

--- 322-Coin-Change/solution.py
+++ 322-Coin-Change/solution.py
@@ -10,18 +10,15 @@ class Solution(object):
         result: return dp[amount]
         assume the initial would be inf 
         """
-        
         dp=[1<<31 for _ in xrange(amount+1)]
-        
         dp[0]=0
-        n=len(coins)
-        for i in xrange(n):
-            for j in xrange(coins[i],amount+1):
+        for coin in coins:
+            for j in xrange(coin,amount+1):
                 # get the min value using coins i
                 # suppose coins[i]==5 , we need check
                 # the min to get to 5, to check the min in dp[0]+1(5 coins)
                 # if we check 6 ,we need know the min num to reach dp[1]+1(5 coins)
-                dp[j]=min(dp[j-coins[i]]+1,dp[j])
+                dp[j]=min(dp[j-coin]+1,dp[j])
                 
         return -1 if dp[amount]==1<<31 else dp[amount]
         
