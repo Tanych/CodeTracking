6ea96dea3865987a3d607f71b471504606dfe600
Victor <Tanych@users.noreply.github.com>
2016-09-16 16:01:06 -0400

    [209. Minimum Size Subarray Sum][Accepted]committed by Victor

--- 209-Minimum-Size-Subarray-Sum/solution.py
+++ 209-Minimum-Size-Subarray-Sum/solution.py
@@ -5,10 +5,10 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+        # two pointer
         left=right=min_len=0
         num_len=len(nums)
         cur_sum=nums[0] if nums else 0
-        
         while right<num_len:
             while cur_sum<s:
                 right+=1
@@ -16,10 +16,9 @@ class Solution(object):
                 if right==num_len:
                     return min_len
                 cur_sum+=nums[right]
-                
             if (right-left+1)<min_len or not min_len:
                 min_len=right-left+1
-                # update left, kick the most left ele
+            # update left, kick the most left ele
             cur_sum-=nums[left]
             left+=1
         return min_len
