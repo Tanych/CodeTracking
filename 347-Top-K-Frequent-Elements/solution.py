4a999624a09be3557b972715eb3d5f45f28f30d7
Victor <Tanych@users.noreply.github.com>
2016-09-16 20:47:34 -0400

    [347. Top K Frequent Elements][Accepted]committed by Victor

--- 347-Top-K-Frequent-Elements/solution.py
+++ 347-Top-K-Frequent-Elements/solution.py
@@ -5,20 +5,4 @@ class Solution(object):
         :type k: int
         :rtype: List[int]
         """
-        n=len(nums)
-        hashmap={} 
-        res=[]
-        # use bucket to store the same frequency key
-        bucket=[[] for _ in xrange(n+1)]
-        # building map
-        for i in xrange(n):
-            hashmap[nums[i]]=hashmap.get(nums[i],0)+1
-        
-        # sort map by value
-        for key,value in hashmap.iteritems():
-            bucket[value].append(key)
-        # the index indicate the frequncy
-        for i in xrange(len(bucket)-1,-1,-1):
-            if len(res)<k and bucket[i]:
-                res+=bucket[i]
-        return res
+        return [x[0] for x in collections.Counter(nums).most_common(k)]
