74132613bf9eefb3b9d149db81ee577b3576bb65
Victor <Tanych@users.noreply.github.com>
2016-09-17 13:47:48 -0400

    [5. Longest Palindromic Substring][Accepted]committed by Victor

--- 5-Longest-Palindromic-Substring/solution.py
+++ 5-Longest-Palindromic-Substring/solution.py
@@ -1,80 +1,9 @@
 class Solution(object):
-    def preprocess(self,s):
-        n=len(s)
-        newstr='^'
-        for ch in s:
-            newstr+='#'+ch
-        newstr+='#$'
-        return newstr
-    
-    def manacher(self,s):
-        newstr=self.preprocess(s)
-        n=len(newstr)
-        pnum=[0]*n
-        center=right=0
-        #get rid of ^,$
-        for i in xrange(1,n-1):
-            i_mirror=center-(i-center)
-            
-            pnum[i]=min(right-i,pnum[i_mirror]) if right>center else 0
-            
-            # expand palindrome centered at i
-            while newstr[i+1+pnum[i]]==newstr[i-1-pnum[i]]:
-                pnum[i]+=1
-            # if palindrome centered at i expand past R, adjust the center to i
-            if i+pnum[i]>right:
-                center=i
-                right=i+pnum[i]
-                
-        # find the max
-        maxlen=centeridx=0
-        for i in xrange(1,n-1):
-            if pnum[i]>maxlen:
-                maxlen=pnum[i]
-                centeridx=i
-                
-        sidx=(centeridx-maxlen-1)/2
-        return s[sidx:sidx+maxlen]
-            
-    def longestPalindromeDP(self,s):
-        """
-        define: dp[i][j] means whether subtring[i,j] is a palindrome
-        initial: dp[i][i] is true, for the single char
-                dp[i][i+1] is true if s[i]==s[i+1]
-        dp[i][j]=dp[i+1][j-1] if s[i]==s[j]
-        """
-        n=len(s)
-        maxstart,maxlen=0,1
-        dp=[[False for _ in xrange(n)] for _ in xrange(n)]
-        
-        for i in xrange(n):
-            dp[i][i]=True
-        for i in xrange(n-1):
-            if s[i]==s[i+1]:
-                dp[i][i+1]=True
-                maxstart=i
-                maxlen=2
-        
-        for k in xrange(3,n+1):
-            # keep the length of palindrom is k, so i stop at n-k+1
-            for i in xrange(n-k+1):
-                # for padlinrome length of k, j start at i+k-1
-                j=i+k-1
-                if s[i]==s[j] and dp[i+1][j-1]:
-                    dp[i][j]=True
-                    maxstart=i
-                    maxlen=k
-        return s[maxstart:maxstart+maxlen]
-            
-                
     def longestPalindrome(self, s):
         """
         :type s: str
         :rtype: str
         """
-        return self.manacher(s)
-        #return self.longestPalindromeDP(s)
-        
         if not s:
             return ""
         str_len=len(s)
