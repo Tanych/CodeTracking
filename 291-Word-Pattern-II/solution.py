12fce71c3655a3642a55668aed998b0bb53e3495
Victor <Tanych@users.noreply.github.com>
2016-09-19 17:46:05 -0400

    [291. Word Pattern II][Accepted]committed by Victor

--- 291-Word-Pattern-II/solution.py
+++ 291-Word-Pattern-II/solution.py
@@ -1,6 +1,7 @@
 class Solution(object):
     def __init__(self):
         self.matching={}
+        # avoid a,b use the same key
         self.strset=set()
     
     def ismatch(self,pattern,i,strs,j):
@@ -8,29 +9,23 @@ class Solution(object):
             return True
         if i==len(pattern) or j==len(strs):
             return False
-        pchar=pattern[i]
-        
-        # exist pattern
-        if pchar in self.matching:
-            matchstr=self.matching[pchar]
-            if not strs.startswith(matchstr,j):
+        p=pattern[i]
+        if p in self.matching:
+            if not strs.startswith(self.matching[p],j):
                 return False
-            return self.ismatch(pattern,i+1,strs,j+len(matchstr))
-            
+            return self.ismatch(pattern,i+1,strs,j+len(self.matching[p]))
         # new pattern
         for k in xrange(j,len(strs)):
             substr=strs[j:k+1]
+            # avoid a,b use same key
             if substr in self.strset:
                 continue
-            
-            self.matching[pchar]=substr
+            self.matching[p]=substr
             self.strset.add(substr)
-            
             if self.ismatch(pattern,i+1,strs,k+1):
                 return True
-            del self.matching[pchar]
+            del self.matching[p]
             self.strset.discard(substr)
-            
         return False
         
     def wordPatternMatch(self, pattern, strs):
