3ebfcc051869c7a95df70add2fa4f3e5fcc030dc
Victor <Tanych@users.noreply.github.com>
2016-09-21 10:34:23 -0400

    [122. Best Time to Buy and Sell Stock II][Accepted]committed by Victor

--- 122-Best-Time-to-Buy-and-Sell-Stock-II/solution.py
+++ 122-Best-Time-to-Buy-and-Sell-Stock-II/solution.py
@@ -4,13 +4,15 @@ class Solution(object):
         :type prices: List[int]
         :rtype: int
         """
+        # find the lowest price to buy
+        # and find the relative high price to sell
         n=len(prices)
         buy=sell=0
         res=0
         while buy<n and sell<n:
-            # find the local small prices
             while buy+1<n and prices[buy+1]<prices[buy]:
                 buy+=1
+            # get the lowest buy,find the highest forward
             sell=buy
             while sell+1<n and prices[sell+1]>prices[sell]:
                 sell+=1
