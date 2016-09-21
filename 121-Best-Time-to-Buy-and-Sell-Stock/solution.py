33c325ded3025e3dded3458ddea1b88d152a9e22
Victor <Tanych@users.noreply.github.com>
2016-09-21 10:23:25 -0400

    [121. Best Time to Buy and Sell Stock][Accepted]committed by Victor

--- 121-Best-Time-to-Buy-and-Sell-Stock/solution.py
+++ 121-Best-Time-to-Buy-and-Sell-Stock/solution.py
@@ -7,6 +7,8 @@ class Solution(object):
         low=1<<31
         profit=0
         for p in prices:
-            low=min(low,p)
-            profit=max(profit,p-low)
+            if p<low:
+                low=p
+            if p-low>profit:
+                profit=p-low
         return profit
