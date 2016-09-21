9d8a7c94317b36ba7ed77e3bf09de4ce5efdf711
Victor <Tanych@users.noreply.github.com>
2016-09-20 21:18:02 -0400

    [316. Remove Duplicate Letters][Accepted]committed by Victor

--- 316-Remove-Duplicate-Letters/solution.py
+++ 316-Remove-Duplicate-Letters/solution.py
@@ -10,20 +10,18 @@ class Solution(object):
         if str_len<=1:
             return s
         
-        for i in range(str_len):
+        for i in xrange(str_len):
             hash_dict[s[i]]=hash_dict.get(s[i],0)+1
 
-        for i in range(str_len):
+        for i in xrange(str_len):
+            # alredy in
             if s[i] in stck:
                 hash_dict[s[i]]-=1
                 continue
-            
             while stck and s[i]<=stck[-1] and hash_dict[stck[-1]]>1:
                 hash_dict[stck[-1]]-=1
                 stck.pop()
-            
             stck.append(s[i])
-  
         return ''.join(stck)
                 
             
