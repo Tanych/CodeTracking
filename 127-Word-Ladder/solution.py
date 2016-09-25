ba24ef08fb8a03332fc8f318fe6a9b3cf6e803b9
Victor <Tanych@users.noreply.github.com>
2016-09-25 16:26:22 -0400

    [127. Word Ladder][Accepted]committed by Victor

--- 127-Word-Ladder/solution.py
+++ 127-Word-Ladder/solution.py
@@ -12,34 +12,34 @@ class Solution(object):
         ewset.add(endWord)
         # start + end
         min_len=2
-        while len(bwset)!=0 and len(ewset)!=0:
+        while bwset and ewset:
             # record all the possible transform in wordList 
-            tw_trans=set()
+            twset=set()
             # BFS try possible solution from begin to end
             for strs in bwset:
                 for i in xrange(len(strs)):
                     # replace with the a-z to check possible solution
                     for k in xrange(ord('a'), ord('z')+1):
-                        com_str=strs[:i]+chr(k)+strs[i+1:]
+                        cmbstr=strs[:i]+chr(k)+strs[i+1:]
                         # if end, return the min_len
-                        if com_str in ewset:
+                        if cmbstr in ewset:
                             return min_len
-                        if com_str in wordList:
-                            tw_trans.add(com_str)
+                        if cmbstr in wordList:
+                            twset.add(cmbstr)
             # wordlist delete the same transform with the words
             # avoid visit the same word mutiple times
-            for li in tw_trans:
-                wordList.remove(li)
+            for li in twset:
+                wordList.discard(li)
             # speedup--
             # everytime mark down the possible path from begin to end
             # reducing time by checking smallest sizes
-            if len(tw_trans)<=len(ewset):
+            if len(twset)<=len(ewset):
                 # move on if the possible solution is less then the ending
-                bwset=tw_trans
+                bwset=twset
             else:
                 # reverse with the small, find one solution in the bigger possible solution
                 bwset=ewset
-                ewset=tw_trans
+                ewset=twset
             min_len+=1
         # no solution
         return 0
