9608aaad6b1e74d43aac83538fbd82c2847681b6
Victor <Tanych@users.noreply.github.com>
2016-09-25 16:55:10 -0400

    [126. Word Ladder II][Accepted]committed by Victor

--- 126-Word-Ladder-II/solution.py
+++ 126-Word-Ladder-II/solution.py
@@ -7,65 +7,50 @@ class Solution(object):
         :rtype: List[List[int]]
         """
         # build the whole graph for the possible solution
-        graph={}
+        graph,res={},[]
+        bwset,ewset=set([beginWord]),set([endWord])
         path=[beginWord]
-        res_paths=[]
-        bwset=set([beginWord])
-        ewset=set([endWord])
-        if self.BFSSearch(bwset,ewset,graph,True,wordlist):
-            self.searchPath(beginWord,endWord,graph,path,res_paths)
-        return res_paths
+        if self.buildGraph(bwset,ewset,graph,True,wordlist):
+            self.searchPath(beginWord,endWord,graph,path,res)
+        return res
      
-    def searchPath(self,beginWord,endWord,graph,path,res_paths):
+    def searchPath(self,beginWord,endWord,graph,path,res):
         if beginWord==endWord:
-            res_paths.append(path)
+            res.append(path)
             return
         # search all possible path
         if beginWord in graph:
             for node in graph[beginWord]:
-                self.searchPath(node,endWord,graph,path+[node],res_paths)
+                self.searchPath(node,endWord,graph,path+[node],res)
                 
     def addPath(self,graph,word,neighor,isfw):
-        """
-        adding the neighor of words to the graph
-        """
         # if the path is forward, adding neighor to word
         if isfw:
             graph[word]=graph.get(word,[])+[neighor]
-        # reverse
         else:
             graph[neighor]=graph.get(neighor,[])+[word]
-    def BFSSearch(self,bwset,ewset,graph,isfw,wordlist):
-        """
-        bwset: beginword set
-        ewset: endword set
-        using BFS search all possible solutions
-        if not found return false
-        """
-        if not len(bwset):
+            
+    def buildGraph(self,bwset,ewset,graph,isfw,wordlist):
+        if not bwset:
             return False
-        # if ewset larger than beset-->reverse to reducing time
-        if len(bwset)>len(ewset):
-            return self.BFSSearch(ewset,bwset,graph,not isfw,wordlist)
-        # remove words already in wordlist
-        for word in (bwset|ewset):
-            wordlist.discard(word)
-        # record all the possible word in next replace
-        twset=set()
-        # mark whether found
-        isFd=False
-        while len(bwset):
+            
+        if len(bwset)<len(ewset):
+            return self.buildGraph(ewset,bwset,graph,not isfw,wordlist)
+        
+        for w in (bwset|ewset):
+            wordlist.discard(w)
+            
+        find,twset=False,set()
+        while bwset:
             word=bwset.pop()
-            for ch in xrange(ord('a'),ord('z')+1):
-                for i in xrange(len(word)):
-                    neighor=word[:i]+chr(ch)+word[i+1:]
-                    if neighor in ewset:
-                        isFd=True
-                        self.addPath(graph,word,neighor,isfw)
-                    # conitue find next level
-                    if not isFd and neighor in wordlist:
-                        twset.add(neighor)
-                        self.addPath(graph,word,neighor,isfw)
-        # move on or return
-        return isFd or self.BFSSearch(twset,ewset,graph,isfw,wordlist)
+            for i in xrange(len(word)):
+                for j in xrange(ord('a'),ord('z')+1):
+                    cmbstr=word[:i]+chr(j)+word[i+1:]
+                    if cmbstr in ewset:
+                        find=True
+                        self.addPath(graph,word,cmbstr,isfw)
+                    if not find and cmbstr in wordlist:
+                        twset.add(cmbstr)
+                        self.addPath(graph,word,cmbstr,isfw)
+        return find or self.buildGraph(twset,ewset,graph,isfw,wordlist)
         
