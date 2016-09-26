36ad0f1003f7ec03a4d2240255fca9c9ba511fdb
Victor <Tanych@users.noreply.github.com>
2016-09-26 10:31:11 -0400

    [78. Subsets][Accepted]committed by Victor

--- 78-Subsets/solution.py
+++ 78-Subsets/solution.py
@@ -5,13 +5,9 @@ class Solution(object):
         :rtype: List[List[int]]
         """
         # try to gernerate all possible value
-        # 2^n
-        nums.sort()
-        res=[]
-        n=len(nums)
+        res,n=[],len(nums)
         for i in xrange(2**n):
             subset=[]
-            # using the binary move
             bn=bin(i)[2:]
             # 001, make all the same length
             bn='0'*(n-len(bn))+bn
