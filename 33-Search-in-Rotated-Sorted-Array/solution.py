3f7d744ced1e30ac4830ab3f265ba64bd5637806
Victor <Tanych@users.noreply.github.com>
2016-09-26 11:23:02 -0400

    [33. Search in Rotated Sorted Array][Accepted]committed by Victor

--- 33-Search-in-Rotated-Sorted-Array/solution.py
+++ 33-Search-in-Rotated-Sorted-Array/solution.py
@@ -5,24 +5,26 @@ class Solution(object):
         :type target: int
         :rtype: int
         """
-        # step1-- first find the pivot of the rotated array
+        #1. nums[left:mid] is consistently increasing
+        #2. nums[mid:right] is consistently increasing
+        
         n=len(nums)
         left,right=0,n-1
         while left<=right:
-            mid=left+(right-left)/2
+            mid=(right+left)/2
             if nums[mid]==target:
                 return mid
-            if nums[mid]==nums[left]:
-                left+=1
-            elif nums[mid]>nums[left]:
-                if target>=nums[left] and target<nums[mid]:
-                    right=mid-1
-                else:
+            if nums[mid]>=nums[left]:
+                # if every num in nums[left:mid] bigger than target or smaller than target
+                if nums[mid]<=target or target<nums[left]:
                     left=mid+1
+                else:
+                    right=mid-1
             else:
-                if target<=nums[right] and target>nums[mid]:
-                    left=mid+1
-                else:
+                # if every num in nums[mid:right] bigger than target or smaller than target
+                if nums[right]<target or target<=nums[mid]:
                     right=mid-1
+                else:
+                    left=mid+1
 
         return -1
