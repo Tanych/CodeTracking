d7fd1fca88ec832c36ec5661c162de52d6e82f76
Victor <Tanych@users.noreply.github.com>
2016-09-26 16:49:06 -0400

    [93. Restore IP Addresses][Time Limit Exceeded]committed by Victor

--- 93-Restore-IP-Addresses/solution.py
+++ 93-Restore-IP-Addresses/solution.py
@@ -1,42 +1,27 @@
 class Solution(object):
-    def checknum(self,s):
-        if not s:
-            return False
+    def isvalid(self,s):
+        n=len(s)
+        if not n: return False
+        val=int(s)
+        if val>255:return False
+        return n==len(str(val))
             
-        intnum=int(s)
-        lens=len(s)
-        if intnum>=0 and intnum<=9 and lens>1:
-            return False
-        if intnum>=10 and intnum<=99 and lens>2:
-            return False
-        if intnum>=100 and intnum<=255 and lens>3:
-            return False
-            
-        return 0<=intnum<=255
-    
-        
-    def helper(self,s,step,ipstr,res):
-        # step means the level to recur
-        # only 4 level
-        if step==3:
-            # if reaches the final level
-            if self.checknum(s):
-                ipstr+='.'+s
-                res.append(ipstr)
-                return 
-        # else continue to recusive find the result
-        for i in xrange(len(s)):
-            # try all possible value in 0-255
-            if self.checknum(s[:i+1]):
-                self.helper(s[i+1:],step+1,ipstr+('.' if ipstr else '')+s[:i+1],res)
-        
     def restoreIpAddresses(self, s):
         """
         :type s: str
         :rtype: List[str]
         """
-        if len(s)<4 or len(s)>12:
-            return []
+        def dfs(strs,step,path,res):
+            if step==3:
+                if self.isvalid(strs):
+                    res.append(path+'.'+strs)
+                    return
+            for i in xrange(len(strs)):
+                unit=strs[:i]
+                if self.isvalid(unit):
+                    divide='.' if path else ''
+                    dfs(strs[i:],step+1,path+divide+unit,res)
+                
         res=[]
-        self.helper(s,0,"",res)
+        dfs(s,0,'',res)
         return res
