cccc79a53ea701204fb5c047a120ee1b9e5f03da
Victor <Tanych@users.noreply.github.com>
2016-09-27 11:15:45 -0400

    [14. Longest Common Prefix][Accepted]committed by Victor

---
+++ 14-Longest-Common-Prefix/solution.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        if not strs:
+            return ''
+            
+        res=""
+        strs.sort()
+        for i in xrange(len(strs[0])):
+            if strs[0][i]==strs[-1][i]:
+                res+=strs[0][i]
+            else:
+                break
+        return res
