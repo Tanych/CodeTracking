668c4bab523fdbbe28b431c593155badc1f0b4d5
Victor <Tanych@users.noreply.github.com>
2016-09-27 11:44:50 -0400

    [19. Remove Nth Node From End of List][Accepted]committed by Victor

--- 19-Remove-Nth-Node-From-End-of-List/solution.py
+++ 19-Remove-Nth-Node-From-End-of-List/solution.py
@@ -11,31 +11,16 @@ class Solution(object):
         :type n: int
         :rtype: ListNode
         """
-        # get the size of head
-        if not head:
-            return None
-        if not head.next:
-            if n==1:
-                return None
-        p=head
-        sizehead=0
-        while p:
-            sizehead+=1
-            p=p.next
-        
-        steps=sizehead-n
-        newhead=ListNode(-1)
-        newhead.next=head
-        pre=newhead
-        cur=head
-        while steps:
-            pre=cur
-            cur=cur.next
-            steps-=1
-        # record the next possible valu
-        cnext=cur.next
-        pre.next=cnext
-        
-        return newhead.next
-        
-        
+        dummpy=ListNode(-1)
+        dummpy.next=head
+        slow=dummpy
+        fast=dummpy
+        # reach on the n node from left
+        for i in xrange(n):
+            fast=fast.next
+        # slow reach on the n-1 node from right
+        while fast.next:
+            slow=slow.next
+            fast=fast.next
+        slow.next=slow.next.next
+        return dummpy.next
